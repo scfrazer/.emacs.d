b9c58bd84cf489366082b096fe96e0c10a966071
Scott Frazer <frazer.scott@gmail.com>
2010-06-15 13:11:49 -0400

    Make default grep file choice *

--- lisp/src/my-grep.el
+++ lisp/src/my-grep.el
@@ -4,7 +4,7 @@
 
 (setq-default grep-highlight-matches t
               grep-find-ignored-directories (list ".git" ".hg" ".bzr" ".svn")
-              grep-files-aliases nil)
+              grep-files-aliases '(("all" . "*")))
 
 (grep-apply-setting 'grep-template "/bin/grep -nH -d skip -I -E -e <R> <C> <F>")
 (grep-apply-setting 'grep-find-template "find <D> <X> -type f <F> -print0 | xargs -0 -e /bin/grep -nH -I -E -e <R> <C>")
