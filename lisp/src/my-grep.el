9d1a24fa21da3a03366c9ed04ad1f76b33cc2650
Scott Frazer <frazer.scott@gmail.com>
2010-07-27 11:08:30 -0400

    Add some grep patterns

--- lisp/src/my-grep.el
+++ lisp/src/my-grep.el
@@ -4,7 +4,12 @@
 
 (setq-default grep-highlight-matches t
               grep-find-ignored-directories (list ".git" ".hg" ".bzr" ".svn")
-              grep-files-aliases '(("all" . "*")))
+              grep-files-aliases '(("all" . "*")
+                                   ("c" . "*.h *.c")
+                                   ("cc" . "*.h *.cc")
+                                   ("e" . "*.e")
+                                   ("el" . "*.el")
+                                   ("v" . "*.v *.sv *.vg")))
 
 (grep-apply-setting 'grep-template "/bin/grep -nH -d skip -I -E -e <R> <C> <F>")
 (grep-apply-setting 'grep-find-template "find <D> <X> -type f <F> -print0 | xargs -0 -e /bin/grep -nH -I -E -e <R> <C>")
