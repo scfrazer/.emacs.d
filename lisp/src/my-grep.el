40785779baf1e9e8ef2a14b7e3e5e3762d069496
Scott Frazer <frazer.scott@gmail.com>
2010-08-17 10:31:21 -0400

    Add more patterns to grep

--- lisp/src/my-grep.el
+++ lisp/src/my-grep.el
@@ -9,7 +9,7 @@
                                    ("cc" . "*.h *.cc")
                                    ("e" . "*.e")
                                    ("el" . "*.el")
-                                   ("v" . "*.v *.sv *.vg")))
+                                   ("v" . "*.v *.sv *.svh *.vg")))
 
 (grep-apply-setting 'grep-template "/bin/grep -nH -d skip -I -E -e <R> <C> <F>")
 (grep-apply-setting 'grep-find-template "find <D> <X> -type f <F> -print0 | xargs -0 -e /bin/grep -nH -I -E -e <R> <C>")
