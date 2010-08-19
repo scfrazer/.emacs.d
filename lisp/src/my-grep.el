4f406b359602b7944503f95e09d881b4d40088da
Scott Frazer <frazer.scott@gmail.com>
2010-08-19 11:12:05 -0400

    grep list change

--- lisp/src/my-grep.el
+++ lisp/src/my-grep.el
@@ -9,7 +9,8 @@
                                    ("cc" . "*.h *.cc")
                                    ("e" . "*.e")
                                    ("el" . "*.el")
-                                   ("v" . "*.v *.sv *.svh *.vg")))
+                                   ("v" . "*.v *.vh *.vg")
+                                   ("sv" . "*.sv *.svh")))
 
 (grep-apply-setting 'grep-template "/bin/grep -nH -d skip -I -E -e <R> <C> <F>")
 (grep-apply-setting 'grep-find-template "find <D> <X> -type f <F> -print0 | xargs -0 -e /bin/grep -nH -I -E -e <R> <C>")
