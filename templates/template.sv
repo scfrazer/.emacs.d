72ec89f2d74c7cd2e72b3edbb532078f95993843
Scott Frazer <frazer.scott@gmail.com>
2010-08-23 11:13:09 -0400

    Company standard SystemVerilog templates

--- templates/template.sv
+++ templates/template.sv
@@ -1,17 +1,26 @@
-// ----------------------------------------------------------------------------
+//--------------------------------------------------------------------------
+//  Cisco Systems Inc.
 //
-//   Title    : %[Title: %]
-//   Filename : %b
-//   Language : SystemVerilog
-//   Author   : %u
+//  Copyright (c) %Y Cisco Systems Inc.
+//  All rights reserved
 //
-//   Description : %1
+//  This software product contains the unpublished source code of
+//  Cisco Systems Inc.
 //
-// ----------------------------------------------------------------------------
-// Copyright (c) %Y by Cisco Systems, Inc
-// This model is the confidential and proprietary property of Cisco and the
-// possession or use of this file requires written permission from Cisco.
-// ----------------------------------------------------------------------------
+//  The copyright notices above do not evidence any actual or
+//  intended publication of such source code.
+//
+//  File Name:          %b
+//
+//  Language:           SystemVerilog
+//
+//  File Description:   %[Description: %]
+//
+//  Initial Version:    %d %M %Y    By: %u
+//
+//--------------------------------------------------------------------------
+//
+// $Id: $
 
 /** @file */
 
--- templates/template.svh
+++ templates/template.svh
@@ -1,20 +1,29 @@
-// ----------------------------------------------------------------------------
+//--------------------------------------------------------------------------
+//  Cisco Systems Inc.
 //
-//   Title    : %[Title: %]
-//   Filename : %b
-//   Language : SystemVerilog
-//   Author   : %u
+//  Copyright (c) %Y Cisco Systems Inc.
+//  All rights reserved
 //
-//   Description : %1
+//  This software product contains the unpublished source code of
+//  Cisco Systems Inc.
 //
-// ----------------------------------------------------------------------------
-// Copyright (c) %Y by Cisco Systems, Inc
-// This model is the confidential and proprietary property of Cisco and the
-// possession or use of this file requires written permission from Cisco.
-// ----------------------------------------------------------------------------
+//  The copyright notices above do not evidence any actual or
+//  intended publication of such source code.
+//
+//  File Name:          %b
+//
+//  Language:           SystemVerilog
+//
+//  File Description:   %[Description: %]
+//
+//  Initial Version:    %d %M %Y    By: %u
+//
+//--------------------------------------------------------------------------
+//
+// $Id: $
 
-`ifndef _%N_%E_
-`define _%N_%E_
+`ifndef _%N_%E_INCLUDED
+`define _%N_%E_INCLUDED
 
 /** @file */
 
