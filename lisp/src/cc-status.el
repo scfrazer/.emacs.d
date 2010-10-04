ba9402fd0ebae19e176745bc7c50715a8701425c
Scott Frazer <frazer.scott@gmail.com>
2010-10-04 10:56:53 -0400

    More work on cc-status

--- lisp/src/cc-status.el
+++ lisp/src/cc-status.el
@@ -1,24 +1,29 @@
 ;;; cc-status.el
 
-;; TODO Checked out files (R/U) can be checked in, or unco -rm/-keep
-;; TODO Uncatalogued files can be deleted or added (mkelem)
-
 (defvar cc-status-item-regexp "^[IUXDA ] [RU ] \\([-a-zA-Z0-9_./]+\\)$"
   "Item regexp.")
 
-(defvar cc-status-dir-name nil
-  "ClearCase status directory name.")
+(defvar cc-status-tree-dir-name nil
+  "ClearCase status tree directory name.")
+(make-variable-buffer-local 'cc-status-tree-dir-name)
 
 ;; Interactive functions
 
 (defun cc-status-tree ()
   "Create ClearCase status tree."
   (interactive)
-  (let ((dir (read-directory-name "ClearCase status for dir? " default-directory nil t)))
-    (pop-to-buffer (get-buffer-create "*cc-status-tree*"))
-    (set (make-local-variable 'cc-status-dir-name) dir)
-    (cc-status-tree-populate)
-    (cc-status-tree-mode)))
+  (let ((buf (get-buffer-create "*cc-status-tree*")))
+    (set-buffer buf)
+    (unless cc-status-tree-dir-name
+      (setq cc-status-tree-dir-name
+            (replace-regexp-in-string
+             "/$" ""
+             (read-directory-name "ClearCase status for dir? "
+                                  default-directory nil t)))
+      (cc-status-tree-refresh)
+      (cc-status-tree-mode))
+    (pop-to-buffer buf)
+    (fit-window-to-buffer nil (/ (frame-height) 2))))
 
 (defun cc-status-mode-next ()
   "Go to next file."
@@ -34,17 +39,18 @@
 
 ;; Non-interactive functions
 
-(defun cc-status-tree-populate ()
-  "Populate the cc-status-mode buffer."
+(defun cc-status-tree-refresh ()
+  "Refresh the cc-status buffer."
   (setq buffer-read-only nil)
   (erase-buffer)
+  (insert cc-status-tree-dir-name ":\n\n")
   (insert "  % Filename\n")
   (insert "  - --------\n")
   (call-process-shell-command (concat
                                "(cleartool lscheckout -me -r -fmt '%n (%Rf)\\n' "
-                               cc-status-dir-name
+                               cc-status-tree-dir-name
                                " ; cleartool lspri -other "
-                               cc-status-dir-name ")") nil t)
+                               cc-status-tree-dir-name ")") nil t)
   (goto-char (point-min))
   (cc-status-mode-next)
   (sort-regexp-fields nil cc-status-item-regexp "\\1" (point) (point-max))
@@ -53,10 +59,10 @@
 
 ;; Map
 
-(defvar cc-status-mode-map nil
+(defvar cc-status-tree-mode-map nil
   "`cc-status-mode' keymap.")
 
-(if (not cc-status-mode-map)
+(if (not cc-status-tree-mode-map)
     (let ((map (make-sparse-keymap)))
       (define-key map (kbd "<down>") 'cc-status-mode-next)
       (define-key map (kbd "<up>") 'cc-status-mode-prev)
@@ -64,7 +70,45 @@
       (define-key map (kbd "C-p") 'cc-status-mode-prev)
       (define-key map "n" 'cc-status-mode-next)
       (define-key map "p" 'cc-status-mode-prev)
-      (setq cc-status-mode-map map)))
+
+      ;; TODO
+      ;; Buffer commands
+      ;; q - bury buffer
+      ;; g - refresh
+      ;; RET - open file
+      ;; j - jump to a different dir
+      ;; x - execute commands
+      ;;
+      ;; Element files/dirs:
+      ;; i - mark for checkin
+      ;; u - mark for unco -keep
+      ;; U - mark for unco -rm
+      ;; = - ediff against predecessor
+      ;;
+      ;; Non-element files/dirs:
+      ;; d - mark for delete
+      ;; a - mark for mkelem
+
+      (setq cc-status-tree-mode-map map)))
+
+;; Font-lock
+
+(defvar cc-status-mode-font-lock-keywords
+  '(
+    ("^  % .+$"
+     (0 font-lock-keyword-face))
+    ("^  - -+$"
+     (0 font-lock-keyword-face))
+    ("^\\(.+\\):$"
+     (1 font-lock-function-name-face))
+    ("^  [RU] .+"
+     (0 font-lock-preprocessor-face))
+    ("^[IA].+"
+     (0 font-lock-variable-name-face))
+    ("^[UXD].+"
+     (0 font-lock-warning-face))
+    )
+  "Keyword highlighting specification for cc-status.")
 
 ;; Mode
 
@@ -73,14 +117,14 @@
 
 Key Bindings:
 
-\\{cc-status-mode-map}"
+\\{cc-status-tree-mode-map}"
   (interactive)
   (setq truncate-lines t)
-  (setq major-mode 'cc-status-mode)
-  (setq mode-name "cc-status")
-  (use-local-map cc-status-mode-map)
-;   (set (make-local-variable 'font-lock-defaults) '(cc-status-mode-font-lock-keywords))
+  (setq major-mode 'cc-status-tree-mode)
+  (setq mode-name "cc-status-tree")
+  (use-local-map cc-status-tree-mode-map)
+  (set (make-local-variable 'font-lock-defaults) '(cc-status-mode-font-lock-keywords))
   (turn-on-font-lock)
-  (run-hooks 'cc-status-mode-hook))
+  (run-hooks 'cc-status-tree-mode-hook))
 
 (provide 'cc-status)
