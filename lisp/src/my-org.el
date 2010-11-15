0bbdf741b3fd4d03ec2fd9944a87abdbf79073a6
Scott Frazer <frazer.scott@gmail.com>
2010-11-15 14:08:51 -0500

    org-mode: Change prefix key

--- lisp/src/my-org.el
+++ lisp/src/my-org.el
@@ -184,7 +184,7 @@ Otherwise: Add a checkbox and update heading accordingly."
 (define-key my-org-mode-map (kbd "y") 'org-paste-subtree)
 
 (defun my-org-mode-hook ()
-  (define-key org-mode-map (kbd "C-;") 'my-org-mode-map)
+  (define-key org-mode-map (kbd "C-f") 'my-org-mode-map)
   (define-key org-mode-map (kbd "C-a") 'my-org-beginning-of-line)
   (font-lock-add-keywords nil '(("OPENED:" (0 'org-special-keyword t))) 'add-to-end))
 
