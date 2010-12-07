6978edd53c0e508c4b12ca7114d7cfef17b51818
Scott Frazer <frazer.scott@gmail.com>
2010-12-07 10:51:04 -0500

    my-reformat: Add option to collect to lines of a given length

--- lisp/src/my-reformat.el
+++ lisp/src/my-reformat.el
@@ -1,17 +1,18 @@
 ;;; my-reformat.el
 
-(defun my-reformat-comma-delimited-items ()
+(defun my-reformat-comma-delimited-items (&optional end-col)
   "Put all comma-delimited items on one line, or each on its own line."
-  (interactive)
-  (my-reformat-delimited-items ","))
+  (interactive "P")
+  (my-reformat-delimited-items "," end-col))
 
-(defun my-reformat-semi-delimited-items ()
+(defun my-reformat-semi-delimited-items (&optional end-col)
   "Put all semi-delimited items on one line, or each on its own line."
-  (interactive)
-  (my-reformat-delimited-items ";"))
+  (interactive "P")
+  (my-reformat-delimited-items ";" end-col))
 
-(defun my-reformat-delimited-items (str)
+(defun my-reformat-delimited-items (str end-col)
   "Put all STR-delimited items on one line, or each on its own line."
+  (interactive "sDelimiting string? \nnEnding column? ")
   (save-excursion
     (backward-up-list)
     (let ((start (1+ (point))) end
@@ -24,7 +25,13 @@
              (replace-regexp-in-string "\n" " " (buffer-substring-no-properties start end)))
             (item-strings '()) pos)
         (with-temp-buffer
-          (insert (replace-regexp-in-string "[ \t]+" " " item-string))
+          (insert item-string)
+          (goto-char (point-min))
+          ;; Trim inner whitespace, unless it's in a string
+          (while (re-search-forward "[ \t]\\{2,\\}" nil t)
+            (unless (nth 3 (syntax-ppss))
+              (replace-match " ")))
+          ;; Split items up
           (goto-char (point-min))
           (setq pos (point))
           (while (not (eobp))
@@ -40,14 +47,31 @@
               (push trimmed-str items)))))
       ;; Collect or disperse items
       (goto-char start)
-      (setq collect (not (= start-line (line-number-at-pos end))))
-      (delete-region start end)
-      (dolist (item items)
-        (insert item str)
-        (if collect
-            (insert " ")
-          (indent-according-to-mode)
-          (insert "\n")))
-      (delete-char -2))))
+      (if end-col
+          (progn
+            (delete-region start end)
+            (unless (string-match "\\s-+$" str)
+              (setq str (concat str " ")))
+            (when items
+              (insert (car items))
+              (setq items (cdr items)))
+            (dolist (item items)
+              (if (<= (+ (current-column) (length str) (length item)) end-col)
+                  (insert str item)
+                (insert str)
+                (skip-syntax-backward " ")
+                (delete-region (point) (line-end-position))
+                (insert "\n" item)
+                (indent-according-to-mode))))
+        (setq collect (not (= start-line (line-number-at-pos end))))
+        (delete-region start end)
+        (dolist (item items)
+          (insert item str)
+          (if collect
+              (insert " ")
+            (indent-according-to-mode)
+            (insert "\n")))
+        (delete-char -1)
+        (delete-char (* -1 (length str)))))))
 
 (provide 'my-reformat)
