1ecde0c8dc5d8702d2ef6e120acc5d04fe92e5ab
Scott Frazer <frazer.scott@gmail.com>
2011-05-17 10:33:34 -0400

    More calculator ops

--- lisp/src/my-calculator.el
+++ lisp/src/my-calculator.el
@@ -5,7 +5,8 @@
 (setq-default calculator-prompt "Calc [%s]: "
               calculator-radix-grouping-separator "_"
               calculator-2s-complement t
-              ;; TODO calculator-user-operators -- >>, <<, log2, w=copy, y=paste
+              calculator-user-operators '(("<" << (lsh TX TY) 2 2)
+                                          (">" >> (lsh TX (* -1 TY)) 2 2)) ;; TODO log2
               ;; TODO calculator-displayers -- separators in decimal mode
               )
 
@@ -14,7 +15,9 @@
   (setq ad-return-value (replace-regexp-in-string "=" "-" ad-return-value)))
 
 (defun my-calculator-mode-hook ()
-  (define-key calculator-mode-map " " nil))
+  (define-key calculator-mode-map " " nil)
+  (define-key calculator-mode-map "w" 'calculator-copy)
+  (define-key calculator-mode-map "y" 'calculator-paste))
 
 (add-hook 'calculator-mode-hook 'my-calculator-mode-hook)
 
