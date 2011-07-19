3f80124c78b084a818e13ff48ecb2459aece3fb9
Scott Frazer <frazer.scott@gmail.com>
2011-07-19 14:25:04 -0400

    my-narrow: Advice narrow-to-region to deactivate the region

--- lisp/src/my-narrow.el
+++ lisp/src/my-narrow.el
@@ -13,30 +13,9 @@
           (recenter))
       (narrow-to-defun))))
 
-;; (defvar my-narrow-header-format "  Narrowed")
-;;
-;; (defadvice narrow-to-region (after my-narrow-to-region activate)
-;;   (when (region-active-p)
-;;     (deactivate-mark)
-;;     (goto-char (point-min)))
-;;   (unless header-line-format
-;;     (setq header-line-format my-narrow-header-format)))
-;;
-;; (defadvice narrow-to-page (after my-narrow-to-page activate)
-;;   (unless header-line-format
-;;     (setq header-line-format my-narrow-header-format)))
-;;
-;; (defadvice narrow-to-defun (after my-narrow-to-defun activate)
-;;   (unless header-line-format
-;;     (setq header-line-format my-narrow-header-format)))
-;;
-;; (defadvice widen (after my-narrow-widen activate)
-;;   (when (equal header-line-format my-narrow-header-format))
-;;       (setq header-line-format nil))
-;;
-;; (ad-remove-advice 'narrow-to-region 'after 'my-narrow-to-region)
-;; (ad-remove-advice 'narrow-to-page 'after 'my-narrow-to-page)
-;; (ad-remove-advice 'narrow-to-defun 'after 'my-narrow-to-defun)
-;; (ad-remove-advice 'widen 'after 'my-narrow-widen)
+(defadvice narrow-to-region (after my-narrow-to-region activate)
+  (when (region-active-p)
+    (deactivate-mark)
+    (goto-char (point-min))))
 
 (provide 'my-narrow)
