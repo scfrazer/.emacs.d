a30a7601244980d3e5623fde1d59b501b30b15b8
Scott Frazer <frazer.scott@gmail.com>
2011-08-08 13:02:48 -0400

    Add abbrev to insert Python super() (w/ arguments) call

--- lisp/src/my-python.el
+++ lisp/src/my-python.el
@@ -16,6 +16,45 @@
 (defun my-python-mode-hook ()
   (flymake-mode 1))
 
+(define-abbrev python-mode-abbrev-table
+  "sup"
+  "super"
+  'my-python-insert-super-call)
+
+(defun my-python-insert-super-call ()
+  (interactive)
+  (let (fn args class)
+    (save-excursion
+      (while (python-beginning-of-block)
+        (when (looking-at "\\s-*def\\s-+\\([a-zA-Z0-9_]+\\)")
+          (setq fn (match-string-no-properties 1))
+          (save-excursion
+            (setq args nil)
+            (search-forward "(")
+            (let ((beg (point))
+                  (end (progn (backward-char) (forward-sexp) (1- (point))))
+                  arg)
+              (goto-char beg)
+              (while (and (< (point) end)
+                          (re-search-forward "\\s-*\\([a-zA-Z0-9_]+\\)" end t))
+                (setq arg (match-string-no-properties 1))
+                (unless (string= arg "self")
+                  (push arg args))
+                (if (looking-at "\\s-*,")
+                    (goto-char (match-end 0))
+                  (unless (looking-at "\\s-*)")
+                    (forward-sexp))))))))
+      (when (looking-at "\\s-*class\\s-+\\([a-zA-Z0-9_]+\\)")
+        (setq class (match-string-no-properties 1))))
+    (when (and fn class)
+      (insert "(" class ", self)." fn "(")
+      (when args
+        (nreverse args)
+        (dolist (arg args)
+          (insert arg ", "))
+        (delete-char -2))
+      (insert ")"))))
+
 (add-hook 'python-mode-hook 'my-python-mode-hook)
 
 (provide 'my-python)
