7bbff8482caa57d916f051ba686626ce18f09bd0
Scott Frazer <frazer.scott@gmail.com>
2011-08-11 13:53:47 -0400

    color theme tweak

--- lisp/src/my-theme.el
+++ lisp/src/my-theme.el
@@ -294,7 +294,7 @@
    (secondary-selection ((t (:background "yellow" :foreground "gray10"))))
    (show-paren-match-face ((t (:background "dodgerblue1" :foreground "white"))))
    (show-paren-mismatch-face ((t (:background "red1" :foreground "white"))))
-   (task-bmk-face ((t (:background "SlateGray2"))))
+   (task-bmk-face ((t (:background "gray85"))))
    (tooltip ((t (:background "lightyellow" :foreground "black"))))
    (trailing-whitespace ((t (:background "#181a26"))))
    (widget-button-pressed-face ((t (:foreground "red"))))
