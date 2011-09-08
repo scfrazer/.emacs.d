24510656f09c409ac1151f9d1e802857f1d199b0
Scott Frazer <frazer.scott@gmail.com>
2011-09-08 13:50:16 -0400

    my-magit: Get passwd from protected file ... kind of dumb but good enough for now.  Maybe figure out which repo is querying and put multiple passwds in there.

--- lisp/src/my-magit.el
+++ lisp/src/my-magit.el
@@ -15,6 +15,20 @@
   (let ((pop-up-windows nil))
     (call-interactively 'magit-status)))
 
+(defun magit-password (proc string)
+  "Checks if git/ssh asks for a password and ask the user for it."
+  (when (or (string-match "^Enter passphrase for key '\\\(.*\\\)': $" string)
+            (string-match "^\\\(.*\\\)'s password:" string))
+    (let ((passwd-file "~/.magit-passwd"))
+      (process-send-string proc
+                           (if (file-exists-p passwd-file)
+                               (with-temp-buffer
+                                 (insert-file-contents passwd-file)
+                                 (buffer-substring-no-properties (point-min) (point-max)))
+                             (concat (read-passwd
+                                      (format "Password for '%s': " (match-string 1 string))
+                                      nil) "\n"))))))
+
 (define-key magit-log-edit-mode-map (kbd "C-x C-s") 'magit-log-edit-commit)
 (define-key magit-mode-map "q" 'my-magit-quit)
 
