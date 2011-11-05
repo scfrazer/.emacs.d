195277f46dec5fe502911a1981ae9a6c631d0f94
Scott Frazer <frazer.scott@gmail.com>
2011-11-05 10:53:03 -0400

    Change OS X settings

--- darwin.el
+++ darwin.el
@@ -1,10 +1,11 @@
 (setq default-frame-alist
-      '((width . 100) (height . 60)))
+      '((width . 120) (height . 60)))
 
 ;; Exec this line to insert the current font as a setting:
 ;; (insert "\n(set-default-font \"" (cdr (assoc 'font (frame-parameters))) "\")\n")
-;; (set-default-font "-apple-Monaco-medium-normal-normal-*-12-*-*-*-m-0-iso10646-1")
-(set-default-font "-apple-Droid_Sans_Mono_Slashed-medium-normal-normal-*-12-*-*-*-m-0-iso10646-1")
+; (set-default-font "-apple-Terminus-medium-normal-normal-*-12-*-*-*-m-0-iso10646-1")
+; (set-default-font "-apple-Terminus-medium-normal-normal-*-14-*-*-*-m-0-iso10646-1")
+(set-default-font "-apple-Fixed-medium-normal-normal-*-14-*-*-*-m-0-iso10646-1")
 
 (setq exec-path (append exec-path (list "/usr/local/bin"
                                         "/usr/local/git/bin"
@@ -19,23 +20,23 @@
 
 ;; Theme
 
-(load-theme 'whiteboard)
+(blue)
 
-(setq-default org-priority-faces '((?A . (:foreground "IndianRed3" :weight bold))
-                                   (?B . (:foreground "SteelBlue3" :weight bold))
-                                   (?C . (:foreground "SpringGreen3" :weight bold)))
-              org-todo-keyword-faces '(("TODO"       . (:foreground "Red3" :weight bold))
-                                       ("STARTED"    . (:foreground "Blue" :weight bold))
-                                       ("WAITING"    . (:foreground "Orange3" :weight bold))
-                                       ("DONE"       . (:foreground "Green4" :weight bold))
-                                       ("MAYBE"      . (:foreground "Cyan4" :weight bold))
-                                       ("SOMEDAY"    . (:foreground "Cyan4" :weight bold))
-                                       ("CANCELED"   . (:foreground "Green4" :weight bold))
-                                       ("REASSIGNED" . (:foreground "Green4" :weight bold))))
-(eval-after-load "org"
-  '(progn
-     (set-face-foreground 'org-tag "RoyalBlue1")
-     (set-face-foreground 'org-checkbox-statistics-todo "HotPink4")
-     (set-face-foreground 'org-checkbox-statistics-done "Green4")
-     (set-face-foreground 'org-special-keyword "DarkOrange4")
-     (set-face-foreground 'org-date "DeepSkyBlue4")))
+;; (setq-default org-priority-faces '((?A . (:foreground "IndianRed3" :weight bold))
+;;                                    (?B . (:foreground "SteelBlue3" :weight bold))
+;;                                    (?C . (:foreground "SpringGreen3" :weight bold)))
+;;               org-todo-keyword-faces '(("TODO"       . (:foreground "Red3" :weight bold))
+;;                                        ("STARTED"    . (:foreground "Blue" :weight bold))
+;;                                        ("WAITING"    . (:foreground "Orange3" :weight bold))
+;;                                        ("DONE"       . (:foreground "Green4" :weight bold))
+;;                                        ("MAYBE"      . (:foreground "Cyan4" :weight bold))
+;;                                        ("SOMEDAY"    . (:foreground "Cyan4" :weight bold))
+;;                                        ("CANCELED"   . (:foreground "Green4" :weight bold))
+;;                                        ("REASSIGNED" . (:foreground "Green4" :weight bold))))
+;; (eval-after-load "org"
+;;   '(progn
+;;      (set-face-foreground 'org-tag "RoyalBlue1")
+;;      (set-face-foreground 'org-checkbox-statistics-todo "HotPink4")
+;;      (set-face-foreground 'org-checkbox-statistics-done "Green4")
+;;      (set-face-foreground 'org-special-keyword "DarkOrange4")
+;;      (set-face-foreground 'org-date "DeepSkyBlue4")))
