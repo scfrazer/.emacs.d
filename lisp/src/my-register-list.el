f7b763cb688b665be2eb17a95fe84ac04f7bbf03
Scott Frazer <frazer.scott@gmail.com>
2011-11-14 15:32:06 -0500

    Add some register-list functions and keys

---
+++ lisp/src/my-register-list.el
@@ -0,0 +1,22 @@
+;;; my-register-list.el
+
+(require 'register-list)
+
+(defun my-register-list-quit ()
+  (interactive)
+  (kill-buffer nil)
+  (when (> (count-windows) 1)
+    (delete-window)))
+
+(defun my-register-list-insert ()
+  (interactive)
+  (beginning-of-line)
+  (when (re-search-forward "^.\\s-+\\([a-z]\\)" (point-at-eol) t)
+    (let ((char (string-to-char (match-string-no-properties 1))))
+      (my-register-list-quit)
+      (insert-register char))))
+
+(define-key register-list-mode-map "q" 'my-register-list-quit)
+(define-key register-list-mode-map "i" 'my-register-list-insert)
+
+(provide 'my-register-list)
