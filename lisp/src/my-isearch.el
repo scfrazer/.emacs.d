ef0dc40497244c39ac826c924897f4babb3a21aa
Scott Frazer <frazer.scott@gmail.com>
2011-11-29 15:34:28 -0500

    Better search forward

--- lisp/src/my-isearch.el
+++ lisp/src/my-isearch.el
@@ -22,16 +22,37 @@
   (interactive)
   (call-interactively 'isearch-forward))
 
+(defun my-isearch-forward-dwim ()
+  "Read char, then isearch-foward-word if alphanumeric,
+or jump forward to input char."
+  (interactive)
+  (message "Forward: ")
+  (let ((char (read-char)))
+    (if (not (string-match "[a-zA-Z0-9_]" (char-to-string char)))
+        (let ((pos (point))
+              (case-fold-search nil))
+          (forward-char)
+          (if (search-forward (char-to-string char) nil t)
+              (backward-char)
+            (goto-char pos)
+            (error "No matching char found.")))
+      (push char unread-command-events)
+      (push ?\< unread-command-events)
+      (push ?\\ unread-command-events)
+      (isearch-forward-regexp))))
+
 (defun my-isearch-mode-hook ()
-  (if (equal this-command 'my-isearch-word-at-point)
-      (isearch-yank-string
-       (buffer-substring-no-properties (progn (skip-syntax-backward "w_") (point))
-                                       (save-excursion (skip-syntax-forward "w_") (point))))
-    (when (and transient-mark-mode mark-active (/= (region-beginning) (region-end)))
-      (when (= (point) (region-end))
-        (exchange-point-and-mark))
-      (isearch-yank-string (buffer-substring (point) (region-end)))
-      (setq mark-active nil))))
+  "Special setup for isearch."
+  (cond ((equal this-command 'my-isearch-word-at-point)
+         (isearch-yank-string
+          (buffer-substring-no-properties (progn (skip-syntax-backward "w_") (point))
+                                          (save-excursion (skip-syntax-forward "w_") (point)))))
+        (t
+         (when (and transient-mark-mode mark-active (/= (region-beginning) (region-end)))
+           (when (= (point) (region-end))
+             (exchange-point-and-mark))
+           (isearch-yank-string (buffer-substring (point) (region-end)))
+           (setq mark-active nil)))))
 
 (add-hook 'isearch-mode-hook 'my-isearch-mode-hook)
 
@@ -45,5 +66,9 @@
 (define-key isearch-mode-map (kbd "C-b") 'isearch-del-char)
 (define-key isearch-mode-map (kbd "C-f") 'isearch-yank-char)
 (define-key isearch-mode-map (kbd "C-y") 'isearch-yank-kill)
+(define-key isearch-mode-map (kbd "C-.") (lambda ()
+                                           (interactive)
+                                           (push ?\* unread-command-events)
+                                           (push ?\. unread-command-events)))
 
 (provide 'my-isearch)
