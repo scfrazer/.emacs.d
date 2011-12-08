370c6cfb5eb796de0cc9fb204c2f7758dd257da9
Scott Frazer <frazer.scott@gmail.com>
2011-12-08 13:57:53 -0500

    Change sv-mode templates

--- templates/template.sv
+++ templates/template.sv
@@ -1,5 +1,22 @@
+//------------------------------------------------------------------------------
+//  Copyright (c) %Y Cisco Systems Inc. All rights reserved.
+//  This software product contains the unpublished source code of
+//  Cisco Systems Inc. The copyright notices above do not evidence
+//  any actual or intended publication of such source code.
+//
+//  File Name:         %b
+//
+//  Language:          System Verilog
+//
+//  File Description:  %@
+//
+//  Initial Version:   %d %M %Y  By: %u
+//
+//------------------------------------------------------------------------------
+//
+// $Id: $
+
 /** @file */
 
 `include "%n.svh"
 
-%@
--- templates/template.svh
+++ templates/template.svh
@@ -1,8 +1,24 @@
+//------------------------------------------------------------------------------
+//  Copyright (c) %Y Cisco Systems Inc. All rights reserved.
+//  This software product contains the unpublished source code of
+//  Cisco Systems Inc. The copyright notices above do not evidence
+//  any actual or intended publication of such source code.
+//
+//  File Name:         %b
+//
+//  Language:          System Verilog
+//
+//  File Description:  %@
+//
+//  Initial Version:   %d %M %Y  By: %u
+//
+//------------------------------------------------------------------------------
+//
+// $Id: $
+
 `ifndef _%N_%E_INCLUDED
 `define _%N_%E_INCLUDED
 
 /** @file */
-
-%@
 
 `endif
