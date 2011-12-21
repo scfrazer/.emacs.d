ebd58afb9cb5bc521b7e3b4bd245d60f17f30dc5
Scott Frazer <frazer.scott@gmail.com>
2011-12-20 21:38:22 -0500

    my-sv-mode: Change some doxygen stuff

--- lisp/src/my-sv-mode.el
+++ lisp/src/my-sv-mode.el
@@ -42,4 +42,53 @@
       (sv-mode-beginning-of-block)
     ad-do-it))
 
+(define-abbrev sv-mode-abbrev-table
+  "tdo"
+  ""
+  (lambda()
+    (insert "//! @todo ")
+    (sv-mode-indent-line)))
+
+(defvar my-sv-mode-uvm-tag "CSCO_SB"
+  "*Tag for uvm messages.")
+
+(defun my-sv-mode-uvm-info (verbosity)
+  (insert "`uvm_info(\"" my-sv-mode-uvm-tag "\", $sformatf(\"TODO\", TODO), " verbosity ");")
+  (sv-mode-indent-line)
+  (beginning-of-line)
+  (search-forward "TODO")
+  (kill-sexp -1))
+
+(define-abbrev sv-mode-abbrev-table
+  "inl"
+  ""
+  (lambda() (my-sv-mode-uvm-info "UVM_LOW")))
+
+(define-abbrev sv-mode-abbrev-table
+  "inm"
+  ""
+  (lambda() (my-sv-mode-uvm-info "UVM_MEDIUM")))
+
+(define-abbrev sv-mode-abbrev-table
+  "inh"
+  ""
+  (lambda() (my-sv-mode-uvm-info "UVM_HIGH")))
+
+(defun my-sv-mode-uvm-err (type)
+  (insert "`uvm_" type "(\"" my-sv-mode-uvm-tag "\", $sformatf(\"TODO\", TODO));")
+  (sv-mode-indent-line)
+  (beginning-of-line)
+  (search-forward "TODO")
+  (kill-sexp -1))
+
+(define-abbrev sv-mode-abbrev-table
+  "err"
+  ""
+  (lambda() (my-sv-mode-uvm-err "error")))
+
+(define-abbrev sv-mode-abbrev-table
+  "fat"
+  ""
+  (lambda() (my-sv-mode-uvm-err "fatal")))
+
 (provide 'my-sv-mode)
