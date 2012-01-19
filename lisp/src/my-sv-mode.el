93e9384877115388e96b6d6f42ff35b6cb5dc91b
Scott Frazer <frazer.scott@gmail.com>
2012-01-19 14:12:31 -0500

    my-sv-mode: Figure out message tag automatically

--- lisp/src/my-sv-mode.el
+++ lisp/src/my-sv-mode.el
@@ -51,33 +51,57 @@
     (insert "//! \\todo ")
     (sv-mode-indent-line)))
 
-(defvar my-sv-mode-uvm-tag "CSCO_SB"
+(defvar my-sv-mode-uvm-tag nil
   "*Tag for uvm messages.")
 
+(make-variable-buffer-local 'my-sv-mode-uvm-tag)
+
+(defun my-sv-mode-guess-tag ()
+  "Try to guess what the uvm message tag should be."
+  ;; Try in file first
+  (unless my-sv-mode-uvm-tag
+    (save-excursion
+      (save-restriction
+        (widen)
+        (goto-char (point-min))
+        (when (re-search-forward "`uvm_\\(info\\|error\\|fatal\\).+?\"\\([a-zA-Z0-9_]+\\)" nil t)
+          (setq my-sv-mode-uvm-tag (match-string-no-properties 2))))))
+  ;; Now look for package
+  (unless my-sv-mode-uvm-tag
+    (let ((filenames (file-expand-wildcards "*_pkg.sv")))
+      (when filenames
+        (string-match "\\(.+?\\)_pkg.sv" (car filenames))
+        (setq my-sv-mode-uvm-tag (upcase (match-string 1 (car filenames)))))))
+  ;; Give up
+  (unless my-sv-mode-uvm-tag
+    (setq my-sv-mode-uvm-tag "TODO")))
+
 (defun my-sv-mode-uvm-info (verbosity)
-  (insert "`uvm_info(\"" my-sv-mode-uvm-tag "\", $sformatf(\"TODO\", TODO), " verbosity ");")
+  (my-sv-mode-guess-tag)
+  (insert "`uvm_info(\"" my-sv-mode-uvm-tag ":TODO\", $sformatf(\"TODO\", TODO), " verbosity ");")
   (sv-mode-indent-line)
   (beginning-of-line)
   (search-forward "TODO")
   (kill-sexp -1))
 
 (define-abbrev sv-mode-abbrev-table
-  "inl"
+  "infl"
   ""
   (lambda() (my-sv-mode-uvm-info "UVM_LOW")))
 
 (define-abbrev sv-mode-abbrev-table
-  "inm"
+  "infm"
   ""
   (lambda() (my-sv-mode-uvm-info "UVM_MEDIUM")))
 
 (define-abbrev sv-mode-abbrev-table
-  "inh"
+  "infh"
   ""
   (lambda() (my-sv-mode-uvm-info "UVM_HIGH")))
 
 (defun my-sv-mode-uvm-err (type)
-  (insert "`uvm_" type "(\"" my-sv-mode-uvm-tag "\", $sformatf(\"TODO\", TODO));")
+  (my-sv-mode-guess-tag)
+  (insert "`uvm_" type "(\"" my-sv-mode-uvm-tag ":TODO\", $sformatf(\"TODO\", TODO));")
   (sv-mode-indent-line)
   (beginning-of-line)
   (search-forward "TODO")
