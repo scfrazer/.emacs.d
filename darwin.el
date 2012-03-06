a717eb4809b685b8a5b0756a75b96b7e851268e5
Scott Frazer <frazer.scott@gmail.com>
2012-03-06 14:03:26 -0500

    OS X changes

--- darwin.el
+++ darwin.el
@@ -1,12 +1,11 @@
 (setq default-frame-alist
-      '((width . 120) (height . 60)))
+      '((width . 100) (height . 60)))
 
 ;; Exec this line to insert the current font as a setting:
-;; (insert "\n(set-default-font \"" (cdr (assoc 'font (frame-parameters))) "\")\n")
-; (set-default-font "-apple-Terminus-medium-normal-normal-*-12-*-*-*-m-0-iso10646-1")
-; (set-default-font "-apple-Terminus-medium-normal-normal-*-14-*-*-*-m-0-iso10646-1")
-(set-default-font "-apple-Fixed-medium-normal-normal-*-14-*-*-*-m-0-iso10646-1")
-(set-face-attribute 'my-buffer-face-mode-face nil :family "Terminus" :height 120)
+;; (insert "\n(set-default-font \"" (cdr (assoc 'font (frame-parameters))) "\")")
+(set-default-font "-apple-Menlo-medium-normal-normal-*-12-*-*-*-m-0-iso10646-1")
+;(set-default-font "-apple-Fixed-medium-normal-normal-*-14-*-*-*-m-0-iso10646-1")
+;(set-face-attribute 'my-buffer-face-mode-face nil :family "Terminus" :height 120)
 
 (setq exec-path (append exec-path (list "/usr/local/bin"
                                         "/usr/local/git/bin"
@@ -14,11 +13,34 @@
                                         "/opt/local/bin")))
 
 (setq-default save-interprogram-paste-before-kill nil
-              select-active-regions nil)
+              select-active-regions nil
+              my-set-cursor-color-read-only-color "yellow3"
+              my-set-cursor-color-normal-color "green3")
 
 (grep-apply-setting 'grep-template "grep -nH -d skip -I -E -e <R> <C> <F>")
 (grep-apply-setting 'grep-find-template "find <D> <X> -type f <F> -print0 | xargs -0 grep -nH -I -E -e <R> <C>")
 
 ;; Theme
 
-(blue)
+(load-theme 'whiteboard)
+
+
+(setq-default org-priority-faces '((?A . (:foreground "IndianRed3" :weight bold))
+                                   (?B . (:foreground "SteelBlue3" :weight bold))
+                                   (?C . (:foreground "SpringGreen3" :weight bold)))
+              org-todo-keyword-faces '(("TODO"       . (:foreground "Red3" :weight bold))
+                                       ("STARTED"    . (:foreground "Blue" :weight bold))
+                                       ("WAITING"    . (:foreground "Orange3" :weight bold))
+                                       ("DONE"       . (:foreground "Green4" :weight bold))
+                                       ("MAYBE"      . (:foreground "Cyan4" :weight bold))
+                                       ("SOMEDAY"    . (:foreground "Cyan4" :weight bold))
+                                       ("CANCELED"   . (:foreground "Green4" :weight bold))
+                                       ("REASSIGNED" . (:foreground "Green4" :weight bold))))
+(eval-after-load "org"
+  '(progn
+     (set-face-foreground 'org-tag "RoyalBlue1")
+     (set-face-foreground 'org-checkbox-statistics-todo "HotPink4")
+     (set-face-foreground 'org-checkbox-statistics-done "Green4")
+     (set-face-foreground 'org-special-keyword "DarkOrange4")
+     (set-face-foreground 'org-date "DeepSkyBlue4")
+     (set-face-foreground 'org-document-title "gold4"))
