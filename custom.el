d511c773d4dd0758675d345cf2d0258d07bc8c19
Scott Frazer <frazer.scott@gmail.com>
2012-03-14 10:20:30 -0400

    Fix ediff colors

--- custom.el
+++ custom.el
@@ -13,14 +13,3 @@
  '(minibuffer-prompt-properties (quote (read-only t point-entered minibuffer-avoid-prompt face minibuffer-prompt)))
  '(safe-local-variable-values (quote ((eval modify-syntax-entry 35 "<") (eval modify-syntax-entry 10 ">") (sgml-always-quote-attributes) (sgml-indent-step . 1) (sgml-indent-data . t) (checkdoc-permit-comma-termination-flag . t) (checkdoc-force-docstrings-flag) (folded-file . t) (clearcase-version-stamp-active . t))))
  '(speedbar-indentation-width 4))
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(ediff-current-diff-A ((((class color) (min-colors 16)) (:background "green4" :foreground "white"))))
- '(ediff-current-diff-B ((t (:background "blue3" :foreground "white"))))
- '(ediff-even-diff-B ((((class color) (min-colors 16)) (:background "Grey50" :foreground "White"))))
- '(ediff-fine-diff-A ((((class color) (min-colors 16)) (:background "dodgerblue4" :foreground "white"))))
- '(ediff-fine-diff-B ((((class color) (min-colors 16)) (:background "seagreen4" :foreground "white"))))
- '(ediff-odd-diff-A ((((class color) (min-colors 16)) (:background "Grey50" :foreground "White")))))
