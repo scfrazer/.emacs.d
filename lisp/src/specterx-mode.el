3861f1faacff4de16f86b4281be9358152b23eb5
Scott Frazer <frazer.scott@gmail.com>
2012-08-22 10:18:11 -0400

    specterx-mode: Don't try to color blk-blk connections

--- lisp/src/specterx-mode.el
+++ lisp/src/specterx-mode.el
@@ -11,19 +11,6 @@
   "Face for SpecterX directives."
   :group 'specterx-mode)
 
-(defun specterx-mode-offset-background-color ()
-  (let* ((bg-color (cdr (assoc 'background-color (frame-parameters))))
-         (vals (mapcar (lambda (val) (lsh val -8)) (color-values bg-color))))
-    (if (eq frame-background-mode 'dark)
-        (setq vals (mapcar (lambda (val) (min 255 (+ val #x1c))) vals))
-      (setq vals (mapcar (lambda (val) (max 0 (- val #x1c))) vals)))
-    (apply 'format "#%02x%02x%02x" vals)))
-
-(defface specterx-block-connect-face
-  (list (list t `(:background ,(specterx-mode-offset-background-color))))
-  "Face for SpecterX block-connect wires."
-  :group 'specterx-mode)
-
 (define-derived-mode specterx-mode sv-mode "SpecterX"
   "Mode for editing Verilog with embedded Perl."
   :abbrev-table nil
@@ -31,9 +18,9 @@
   (let ((keywords (list
                    (cons "\\(<%\\|%>\\|@perl\\)"
                          '(0 'specterx-directive-face t))
-                   (cons "[a-zA-Z0-9_]+__[a-zA-Z0-9_]+__"
-                         '(0 'specterx-block-connect-face t))
-                   (cons "[^/]\\([*%]\\)[^/]"
+                   (cons "[a-zA-Z0-9_]\\([*%]\\)"
+                         '(1 'specterx-directive-face t))
+                   (cons "\\([*%]\\)[a-zA-Z0-9_]"
                          '(1 'specterx-directive-face t))
                    (cons (concat "specter\\s-+\\("
                                  (regexp-opt '("test" "init_regs" "fixport" "fixpins"
