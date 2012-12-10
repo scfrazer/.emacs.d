c87317ecb7f91ab5110d67fd714481508a358900
Scott Frazer <frazer.scott@gmail.com>
2012-12-10 14:47:36 -0500

    Switch to my own version of tmux copying

--- lisp/src/my-tmux.el
+++ lisp/src/my-tmux.el
@@ -1,14 +1,33 @@
 ;;; my-tmux.el
 
-(require 'emamux)
-
 (setq-default interprogram-cut-function 'my-interprogram-cut-function)
 
+(defvar my-tmux-max-copy-length 5000
+  "*Max length of string to copy to tmux in `my-interprogram-cut-function'.")
+
 (defun my-interprogram-cut-function (text)
-  "If in a tmux session, also copy TEXT to a tmux buffer."
+  "Don't copy if in a keyboard macro, and if in a tmux session also copy TEXT to a tmux buffer (if it's not too big)."
   (unless executing-kbd-macro
-    (when (getenv "TMUX")
-      (emamux:set-buffer text 0))
-    (x-select-text text)))
+    (x-select-text text)
+    (when (and (getenv "TMUX")
+               (<= (length text) my-tmux-max-copy-length))
+      (my-tmux-copy text))))
+
+(defun my-tmux-copy-region (beg end)
+  "Copy region to tmux buffer."
+  (interactive "r")
+  (my-tmux-copy (buffer-substring-no-properties beg end)))
+
+(defun my-tmux-copy (text)
+  "Copy text to tmux buffer."
+  (call-process-shell-command
+   (format "tmux set-buffer -- \"%s\""
+           (replace-regexp-in-string
+            "\\$" "\"'$'\""
+            (replace-regexp-in-string
+             "\\\"" "\"'\"'\""
+             (replace-regexp-in-string
+              "!" "\\\\!"
+              (replace-regexp-in-string "\n" "\\\\\n" text))))) nil 0))
 
 (provide 'my-tmux)
