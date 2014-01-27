37d2a774b177c1da97961781e6b02a8a145c94a9
Scott Frazer <frazer.scott@gmail.com>
2014-01-26 21:47:15 -0500

    Tweak ido-vertical mode and themes

--- lisp/src/ido-vertical-mode.el
+++ lisp/src/ido-vertical-mode.el
@@ -30,10 +30,10 @@
 (require 'ido)
 
 (defvar ido-vertical-decorations
-  '("\n=> "                             ; left bracket around prospect list
+  '("\n> "                             ; left bracket around prospect list
     ""                                  ; right bracket around prospect list
-    "\n   "                             ; separator between prospects, depends on `ido-separator`
-    "\n   ..."                          ; inserted at the end of a truncated list of prospects
+    "\n  "                             ; separator between prospects, depends on `ido-separator`
+    "\n  ..."                          ; inserted at the end of a truncated list of prospects
     "["                                 ; left bracket around common match string
     "]"                                 ; right bracket around common match string
     " [No match]"
@@ -41,7 +41,7 @@
     " [Not readable]"
     " [Too big]"
     " [Confirm]"
-    "\n=> "                             ; left bracket around the sole remaining completion
+    "\n> "                             ; left bracket around the sole remaining completion
     ""                                  ; right bracket around the sole remaining completion
     )
 
