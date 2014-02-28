6c238207f7524e3d9a99e285b321e144dd21c59d
Scott Frazer <frazer.scott@gmail.com>
2014-02-28 15:18:06 -0500

    Try ibuffer instead of bs

--- lisp/src/my-ibuffer.el
+++ lisp/src/my-ibuffer.el
@@ -2,10 +2,36 @@
 
 (require 'ibuffer)
 
-(setq-default ibuffer-default-shrink-to-minimum-size t
-              ibuffer-default-sorting-mode 'filename-and-dired
+(setq-default ibuffer-default-sorting-mode 'filename-and-dired
               ibuffer-display-summary nil
+              ibuffer-filter-group-name-face 'font-lock-keyword-face
+              ibuffer-never-show-predicates '("TAGS$")
+              ibuffer-expert t
+              ibuffer-show-empty-filter-groups nil
               ibuffer-use-other-window t)
+
+(add-to-list 'ibuffer-formats
+             '(mark modified read-only "  " (name 18 18 :left :elide) "  " (mode 16 16 :left :elide) "  " filename-and-process))
+
+(defvar my-ibuffer-vc-regexp (regexp-opt (list "*cc-status"
+                                               "*clearcase-config-spec"
+                                               "*magit:")))
+
+(defvar my-ibuffer-star-regexp (regexp-opt (list "*Find"
+                                                 "*Occur"
+                                                 "*calculator"
+                                                 "*compilation"
+                                                 "*grep"
+                                                 "*info"
+                                                 "*shell"
+                                                 "*terminal"
+                                                 "*scratch")))
+(setq ibuffer-fontification-alist
+      `((4 (eq major-mode 'dired-mode) font-lock-type-face)
+        (3 (or (string-match (concat my-ibuffer-vc-regexp "\\|" my-ibuffer-star-regexp) (buffer-name))
+               (eq major-mode 'Custom-mode)) font-lock-type-face)
+        (2 (string-match "^*" (buffer-name)) font-lock-comment-face)
+        (1 buffer-read-only font-lock-doc-face)))
 
 (define-ibuffer-sorter filename-and-dired
   "Sort the buffers by their pathname."
@@ -24,6 +50,18 @@
          ;; so that all non pathnames are at the end
          "~"))))
 
+(setq ibuffer-saved-filter-groups
+      `(("default"
+         ("VC" (name . my-ibuffer-vc-regexp))
+         ("Org" (mode . org-mode))
+         ("ELisp" (mode . emacs-lisp-mode))
+         ("VOB" (filename . "/vob"))
+         ("Files" (or (mode . dired-mode)
+                      (filename . "/[^v][^o][^b]")))
+         ("*" (or (mode . Custom-mode)
+                  (name . my-ibuffer-star-regexp)))
+         )))
+
 (defun my-ibuffer ()
   "Open ibuffer with point on last buffer name."
   (interactive)
@@ -32,6 +70,8 @@
     (ibuffer-jump-to-buffer buf)))
 
 (defun my-ibuffer-mode-hook ()
+  (ibuffer-auto-mode 1)
+  (ibuffer-switch-to-saved-filter-groups "default")
   (define-key ibuffer-mode-map (kbd "s f") 'ibuffer-do-sort-by-filename-and-dired)
   (define-key ibuffer-mode-map (kbd "RET") 'ibuffer-visit-buffer-1-window)
   (hl-line-mode 1))
