4fbb6ef23ca4652bacea3f13d9c6dcadd8ee87c7
Scott Frazer <frazer.scott@gmail.com>
2014-06-25 23:11:14 -0400

    my-debug: Add commands to go to next/previous DEBUG

--- lisp/src/my-debug.el
+++ lisp/src/my-debug.el
@@ -1,8 +1,5 @@
 ;;; my-debug.el
 
-;; TODO
-;; v -> grep version-controlled files
-
 (require 'll-debug)
 
 (setq-default ll-debug-output-prefix (concat "DEBUG-" (getenv "USER") "-")
@@ -50,11 +47,26 @@
       (goto-char beg)
       (my-debug-insert-line))))
 
+(defun my-debug-next ()
+  "Go to next DEBUG statement."
+  (interactive)
+  (forward-char 1)
+  (if (re-search-forward (concat "DEBUG$\\|" ll-debug-output-prefix) nil t)
+      (goto-char (match-beginning 0))
+    (backward-char 1)
+    (error "No more debug statements")))
+
+(defun my-debug-previous ()
+  "Go to previous DEBUG statement."
+  (interactive)
+  (unless (re-search-backward (concat "DEBUG$\\|" ll-debug-output-prefix) nil t)
+    (error "No more debug statements")))
+
 (defun my-debug-occur ()
   "Run occur for DEBUG in the current buffer."
   (interactive)
   (let ((case-fold-search nil))
-    (occur (concat (concat "DEBUG$\\|" ll-debug-output-prefix)))))
+    (occur (concat "DEBUG$\\|" ll-debug-output-prefix))))
 
 (defun my-debug-multi-occur ()
   "Run occur for DEBUG in all buffers."
@@ -68,8 +80,10 @@
 (define-key my-debug-map (kbd "R") 'll-debug-revert)
 (define-key my-debug-map (kbd "c") 'my-debug-comment-region)
 (define-key my-debug-map (kbd "d") 'my-debug-insert-line)
-(define-key my-debug-map (kbd "l") 'my-debug-insert-ll)
+(define-key my-debug-map (kbd "i") 'my-debug-insert-ll)
+(define-key my-debug-map (kbd "n") 'my-debug-next)
 (define-key my-debug-map (kbd "o") 'my-debug-occur)
+(define-key my-debug-map (kbd "p") 'my-debug-previous)
 (define-key my-debug-map (kbd "r") 'll-debug-renumber)
 
 (provide 'my-debug)
