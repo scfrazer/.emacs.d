3a2dcf3e036dfb3f1a12e9c14febe88bd080fa9d
Scott Frazer <frazer.scott@gmail.com>
2015-03-27 15:11:06 -0400

    Usable ur-log-mode

--- lisp/src/ur-log-mode.el
+++ lisp/src/ur-log-mode.el
@@ -1,10 +1,37 @@
 ;;; ur-log-mode.el
 
+(defvar ur-log-mode-font-lock-keywords
+  '(
+    ("#.+$"
+     (0 'font-lock-comment-face))
+    ("^------.+$"
+     (0 'font-lock-warning-face))
+    )
+  "Font locking for 'ur-log-mode'.")
+
 (defvar ur-log-mode-syntax-table
   (let ((table (copy-syntax-table text-mode-syntax-table)))
     (modify-syntax-entry ?#  "<" table)
+    (modify-syntax-entry ?\n ">" table)
     table)
   "Syntax table used in ur-log-mode buffers.")
+
+(defun ur-log-mode-insert-first-comment ()
+  "Insert comment from first file."
+  (interactive)
+  (let (beg end comment)
+    (save-excursion
+      (goto-char (point-min))
+      (when (and (search-forward "---------- Only add comments above this line. All other edits will be lost. ----------" nil t)
+                 (re-search-forward "^\\s-+/vob/sse" nil t))
+        (forward-line 1)
+        (setq beg (point))
+        (while (and (not (eobp)) (not (looking-at "^\\s-*$")))
+          (forward-line 1))
+        (setq comment (buffer-substring-no-properties beg (point)))))
+    (when comment
+      (delete-blank-lines)
+      (insert (replace-regexp-in-string "^\\s-*" "" comment)))))
 
 (defun ur-log-mode-commit ()
   "Save and exit."
@@ -15,9 +42,15 @@
 (define-derived-mode ur-log-mode text-mode "ur-log"
   :abbrev-table nil
   :syntax-table ur-log-mode-syntax-table
-  (setq comment-start "#")
+  (make-local-variable 'comment-start)
+  (make-local-variable 'comment-end)
+  (setq comment-start "#"
+        comment-end "")
+  (make-local-variable 'font-lock-defaults)
+  (setq font-lock-defaults '(ur-log-mode-font-lock-keywords t))
   (turn-on-font-lock)
   (font-lock-fontify-buffer)
+  (local-set-key (kbd "C-c C-l") 'ur-log-mode-insert-first-comment)
   (local-set-key (kbd "C-c C-c") 'ur-log-mode-commit))
 
 (add-to-list 'auto-mode-alist '("\\.txt\\.edit\\.[0-9]+\\'" . ur-log-mode))
