acbc1358237de67a15750452b1d11c295406e2ca
Scott Frazer <frazer.scott@gmail.com>
2016-01-20 11:04:02 -0500

    Update from ELPA

--- elpa/command-frequency-1.1/command-frequency-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; command-frequency-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("command-frequency-pkg.el" "command-frequency.el")
-;;;;;;  (20135 7469 901207))
-
-;;;***
-
-(provide 'command-frequency-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; command-frequency-autoloads.el ends here
