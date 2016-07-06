bef933fe9de524f974a7f57ffb63c0310452fb34
Scott Frazer <frazer.scott@gmail.com>
2016-07-06 08:04:36 -0700

    Update sqlup-mode

--- lisp/src/sqlup-mode.el
+++ lisp/src/sqlup-mode.el
@@ -5,8 +5,8 @@
 ;; Author: Aldric Giacomoni <trevoke@gmail.com>
 ;; URL: https://github.com/trevoke/sqlup-mode.el
 ;; Created: Jun 25 2014
-;; Version: 0.1.1
-;; Keywords: sql, tools
+;; Version: 0.6.0
+;; Keywords: sql, tools, redis, upcase
 
 ;;; License:
 
@@ -27,54 +27,223 @@
 
 ;;; Commentary:
 
-;; Activate the minor mode (M-x sqlup-mode) and type away
-;; Alternatively, use a hook: (add-hook 'sql-mode 'sqlup-mode)
-;; The capitalization is triggered when you press 'SPC', ';' or '('
+;; `M-x sqlup-mode` and just type.
+;; This mode supports the various built-in SQL modes as well as redis-mode.
+;; The capitalization is triggered when you press the following keys:
+;; * SPC
+;; * RET
+;; * ,
+;; * ;
+;; * (
+;; * '
 ;;
-;; This mode also provides a function to capitalize SQL keywords inside a region.
-;; It is not bound to a keybinding, but it is available even without activating the mode (which might be a bug)
+;; This package also provides a function to capitalize SQL keywords inside a
+;; region - always available, no need to activate the minor mode to use it:
+;;
 ;; M-x sqlup-capitalize-keywords-in-region
-;; (local-set-key (kbd "C-c u") 'sqlup-capitalize-keywords-in-region)
+;;
+;; It is not bound to a keybinding. Here is an example of how you could do it:
+;;
+;; (global-set-key (kbd "C-c u") 'sqlup-capitalize-keywords-in-region)
+;;
+;; Here follows an example setup to activate `sqlup-mode` automatically:
+;;
+;; (add-hook 'sql-mode-hook 'sqlup-mode)
+;; (add-hook 'sql-interactive-mode-hook 'sqlup-mode)
+;; (add-hook 'redis-mode-hook 'sqlup-mode)
+
+
 
 ;;; Code:
 
-(defvar sqlup-keywords
-  '("absolute" "action" "add" "after" "all" "allocate" "alter" "and" "any" "are" "array" "as" "asc" "asensitive" "assertion" "asymmetric" "at" "atomic" "authorization" "avg" "before" "begin" "between" "bigint" "binary" "bit" "bitlength" "blob" "boolean" "both" "breadth" "by" "call" "called" "cascade" "cascaded" "case" "cast" "catalog" "char" "char_length" "character" "character_length" "check" "clob" "close" "coalesce" "collate" "collation" "column" "commit" "condition" "connect" "connection" "constraint" "constraints" "constructor" "contains" "continue" "convert" "corresponding" "count" "create" "cross" "cube" "current" "current_date" "current_default_transform_group" "current_path" "current_role" "current_time" "current_timestamp" "current_transform_group_for_type" "current_user" "cursor" "cycle" "data" "date" "day" "deallocate" "dec" "decimal" "declare" "default" "deferrable" "deferred" "delete" "depth" "deref" "desc" "describe" "descriptor" "deterministic" "diagnostics" "disconnect" "distinct" "do" "domain" "double" "drop" "dynamic" "each" "element" "else" "elseif" "end" "equals" "escape" "except" "exception" "exec" "execute" "exists" "exit" "external" "extract" "false" "fetch" "filter" "first" "float" "for" "foreign" "found" "free" "from" "full" "function" "general" "get" "global" "go" "goto" "grant" "group" "grouping" "handler" "having" "hold" "hour" "identity" "if" "immediate" "in" "indicator" "initially" "inner" "inout" "input" "insensitive" "insert" "int" "integer" "intersect" "interval" "into" "is" "isolation" "iterate" "join" "key" "language" "large" "last" "lateral" "leading" "leave" "left" "level" "like" "limit" "local" "localtime" "localtimestamp" "locator" "loop" "lower" "map" "match" "map" "member" "merge" "method" "min" "minute" "modifies" "module" "month" "multiset" "names" "national" "natural" "nchar" "nclob" "new" "next" "no" "none" "not" "null" "nullif" "numeric" "object" "octet_length" "of" "old" "on" "only" "open" "option" "or" "order" "ordinality" "out" "outer" "output" "over" "overlaps" "pad" "parameter" "partial" "partition" "path" "position" "precision" "prepare" "preserve" "primary" "prior" "privileges" "procedure" "public" "range" "read" "reads" "real" "recursive" "ref" "references" "referencing" "relative" "release" "repeat" "resignal" "restrict" "result" "return" "returns" "revoke" "right" "role" "rollback" "rollup" "routine" "row" "rows" "savepoint" "schema" "scope" "scroll" "search" "second" "section" "select" "sensitive" "session" "session_user" "set" "sets" "signal" "similar" "size" "smallint" "some" "space" "specific" "specifictype" "sql" "sqlcode" "sqlerror" "sqlexception" "sqlstate" "sqlwarning" "start" "state" "static" "submultiset" "substring" "sum" "symmetric" "system" "system_user" "table" "tablesample" "temporary" "then" "time" "timestamp" "timezone_hour" "timezone_minute" "to" "trailing" "transaction" "translate" "translation" "treat" "trigger" "trim" "true" "under" "undo" "union" "unique" "unknown" "unnest" "until" "update" "upper" "usage" "user" "using" "value" "values" "varchar" "varying" "view" "when" "whenever" "where" "while" "window" "with" "within" "without" "work" "write" "year" "zone")
-  "This is a list of SQL keywords from the SQL standard")
+(require 'cl)
 
-(defun sqlup-maybe-capitalize-and-insert (str)
-  (interactive)
-  (sqlup-maybe-capitalize-word-at-point)
-  (insert str))
+(defconst sqlup-trigger-characters
+  (mapcar 'string-to-char '(";"
+                            " "
+                            "("
+                            ","
+                            "''"))
+  "When the user types one of these characters,
+this mode's logic will be evaluated.")
 
-(defun sqlup-maybe-capitalize-word-at-point ()
-  (when (looking-back "[a-zA-Z0-9_]+" (point-at-bol) t)
-    (let ((word (match-string-no-properties 0)))
-      (when (member (downcase word) sqlup-keywords)
-        (replace-match (upcase word) t t)))))
+(defconst sqlup-eval-keywords
+  '((postgres "EXECUTE" "format("))
+  "List of keywords introducing eval strings, organised by dialect.")
 
-;;;###autoload
-(defun sqlup-capitalize-keywords-in-region ()
-  "Call this function on a region to capitalize the SQL keywords therein."
-  (interactive "*")
-  (let ((beg (region-beginning))
-        (end (region-end)))
-    (save-excursion
-      (goto-char beg)
-      (while (search-forward-regexp "[a-zA-Z0-9_]+" end t)
-        (when (member (downcase (match-string 0)) sqlup-keywords)
-          (replace-match (upcase (match-string 0)) t t))))))
+(defvar sqlup-local-keywords nil
+  "Buffer-local variable holding regexps to identify keywords.")
+
+(defvar sqlup-work-buffer nil
+  "Buffer-local variable keeping track of the name of the buffer where sqlup
+figures out what is and isn't a keyword.")
 
 ;;;###autoload
 (define-minor-mode sqlup-mode
   "Capitalizes SQL keywords for you."
   :lighter " SUP"
-  :keymap (let ((map (make-sparse-keymap)))
-            (define-key map (kbd "SPC") (lambda () (interactive) (sqlup-maybe-capitalize-and-insert " ")))
-            (define-key map (kbd "(") (lambda () (interactive) (sqlup-maybe-capitalize-and-insert "(")))
-            (define-key map (kbd ")") (lambda () (interactive) (sqlup-maybe-capitalize-and-insert ")")))
-            (define-key map (kbd ";") (lambda () (interactive) (sqlup-maybe-capitalize-and-insert ";")))
-            (define-key map (kbd ",") (lambda () (interactive) (sqlup-maybe-capitalize-and-insert ",")))
-            map))
+  (if sqlup-mode
+      (sqlup-enable-keyword-capitalization)
+    (sqlup-disable-keyword-capitalization)))
+
+(defun sqlup-enable-keyword-capitalization ()
+  "Add buffer-local hook to handle this mode's logic"
+  (set (make-local-variable 'sqlup-work-buffer) nil)
+  (set (make-local-variable 'sqlup-local-keywords) nil)
+  (set (make-local-variable 'sqlup-last-sql-keyword) nil)
+  (add-hook 'post-command-hook 'sqlup-capitalize-as-you-type nil t))
+
+(defun sqlup-disable-keyword-capitalization ()
+  "Remove buffer-local hook to handle this mode's logic"
+  (kill-buffer (sqlup-work-buffer))
+  (remove-hook 'post-command-hook 'sqlup-capitalize-as-you-type t))
+
+(defun sqlup-capitalize-as-you-type ()
+  "If the user typed a trigger key, check if we should capitalize
+the previous word."
+  (if (sqlup-should-do-work-p)
+      (save-excursion (sqlup-maybe-capitalize-symbol -1))))
+
+(defun sqlup-should-do-work-p ()
+  "Checks whether the user pressed one of the trigger keys.
+Other than <RET>, characters are in variable sqlup-trigger-characters."
+  (and (sqlup-not-just-initialized-p)
+       (or (sqlup-user-pressed-return-p)
+           (and (sqlup-user-is-typing-p)
+                (sqlup-trigger-self-insert-character-p)))))
+
+(defun sqlup-not-just-initialized-p ()
+  (not (eq this-command 'sqlup-mode)))
+
+(defun sqlup-user-pressed-return-p ()
+  (and (< 0 (length (this-command-keys-vector)))
+       (or (equal 13 (elt (this-command-keys-vector) 0))
+           (equal 10 (elt (this-command-keys-vector) 0)))))
+
+(defun sqlup-user-is-typing-p ()
+  (eq this-command #'self-insert-command))
+
+(defun sqlup-trigger-self-insert-character-p ()
+  (let ((sqlup-current-char (elt (this-command-keys-vector) 0)))
+    (member sqlup-current-char sqlup-trigger-characters)))
+
+(defun sqlup-maybe-capitalize-symbol (direction)
+  "DIRECTION is either 1 for forward or -1 for backward"
+  (forward-symbol direction)
+  (sqlup-work-on-symbol (thing-at-point 'symbol)
+                        (bounds-of-thing-at-point 'symbol)))
+
+(defun sqlup-work-on-symbol (symbol symbol-boundaries)
+  (if (and symbol
+           (sqlup-keyword-p (downcase symbol))
+           (sqlup-capitalizable-p (point)))
+      (progn
+        (upcase-region (car symbol-boundaries)
+                       (cdr symbol-boundaries)))))
+
+(defun sqlup-match-eval-keyword-p (dialect)
+  "Return t if the code just before point ends with an eval keyword valid in
+the given DIALECT of SQL."
+  (some 'identity
+        (mapcar #'(lambda (kw) (looking-back (concat kw "[\s\n\r\t]*")))
+                (cdr (assoc dialect sqlup-eval-keywords)))))
+
+(defun sqlup-in-eval-string-p (dialect)
+  "Return t if we are in an eval string."
+  (save-excursion
+    (if (sqlup-string-p)
+        (progn
+          (goto-char (nth 8 (syntax-ppss)))
+          (sqlup-match-eval-keyword-p dialect)))))
+
+(defun sqlup-capitalizable-p (point-location)
+  (let ((dialect (sqlup-valid-sql-product)))
+    (with-current-buffer (sqlup-work-buffer)
+      (goto-char point-location)
+      (and (not (sqlup-comment-p))
+           (not (and (not (sqlup-in-eval-string-p dialect))
+                     (sqlup-string-p)))))))
+
+(defun sqlup-comment-p ()
+  (and (nth 4 (syntax-ppss)) t))
+
+(defun sqlup-string-p ()
+  (and (nth 3 (syntax-ppss)) t))
+
+;;;###autoload
+(defun sqlup-capitalize-keywords-in-region (start-pos end-pos)
+  "Call this function on a region to capitalize the SQL keywords therein."
+  (interactive "r")
+  (save-excursion
+    (goto-char start-pos)
+    (while (< (point) end-pos)
+      (sqlup-maybe-capitalize-symbol 1))))
+
+(defun sqlup-keywords-regexps ()
+  (or sqlup-local-keywords
+      (set (make-local-variable 'sqlup-local-keywords)
+           (sqlup-find-correct-keywords))))
+
+(defun sqlup-find-correct-keywords ()
+  "Depending on the major mode (redis-mode or sql-mode), find the
+correct keywords. If not, create a (hopefully sane) default based on
+ANSI SQL keywords."
+  (cond ((sqlup-redis-mode-p) (mapcar 'downcase redis-keywords))
+        ((sqlup-within-sql-buffer-p) (mapcar 'car sql-mode-font-lock-keywords))
+        (t (mapcar 'car (sql-add-product-keywords
+                         (sqlup-valid-sql-product) '())))))
+
+(defun sqlup-valid-sql-product ()
+  (or (and (boundp 'sql-product)
+           sql-product)
+      'ansi))
+
+(defun sqlup-redis-mode-p ()
+  (eq major-mode #'redis-mode))
+
+(defun sqlup-within-sql-buffer-p ()
+  (and (boundp 'sql-mode-font-lock-keywords) sql-mode-font-lock-keywords))
+
+(defun sqlup-keyword-p (word)
+  (let* ((sqlup-keyword-found nil)
+         (sqlup-terms (sqlup-keywords-regexps))
+         (sqlup-term (car sqlup-terms)))
+    (while (and (not sqlup-keyword-found)
+                sqlup-terms)
+      (setq sqlup-keyword-found (string-match sqlup-term word))
+      (setq sqlup-term (car sqlup-terms))
+      (setq sqlup-terms (cdr sqlup-terms)))
+    (and sqlup-keyword-found t)))
+
+(defun sqlup-work-buffer ()
+  "Returns and/or creates an indirect buffer based on current buffer and set
+its major mode to sql-mode"
+  (or sqlup-work-buffer
+      (set (make-local-variable 'sqlup-work-buffer)
+           (with-current-buffer (clone-indirect-buffer
+                                 (generate-new-buffer-name
+                                  (format "*sqlup-%s*" (buffer-name)))
+                                 nil)
+             (sql-mode)
+             (current-buffer)))))
+
+(defadvice font-lock-mode (around sqlup-ignore-font-lock-on-indirect-buffer activate)
+  "Do not turn on jit-lock-mode on indirect buffers at all.
+Because we're using indirect buffers, the font face gets shared and when we
+change the major mode in the indirect buffer it messes with the font in the
+base buffer (the one the user cares about). This tells emacs to not enable
+font locking in an indirect buffer for which the primary buffer has
+sqlup-mode enabled."
+  (unless (and (buffer-base-buffer)
+               (with-current-buffer (buffer-base-buffer)
+                 sqlup-mode))
+    ad-do-it))
+
+(defadvice sql-set-product (after sqlup-invalidate-sqlup-keyword-cache activate)
+  "Advice sql-set-product, to invalidate sqlup's keyword cache after changing
+the sql product. We need to advice sql-set-product since sql-mode does not
+provide any hook that runs after changing the product"
+  (setq sqlup-local-keywords nil))
 
 (provide 'sqlup-mode)
+;;; sqlup-mode.el ends here
