c7ad7fa07cd4aa7b3054fadac6633077684d4316
Scott Frazer <frazer.scott@gmail.com>
2016-11-04 06:10:08 -0700

    Add another theme

---
+++ themes/smf-zenburn-theme.el
@@ -0,0 +1,22 @@
+(deftheme smf-zenburn
+  "Zenburn colors.")
+
+(load-theme 'smf-base-dark t)
+
+(custom-theme-set-faces
+ 'smf-zenburn
+
+ `(font-lock-builtin-face       ((t :foreground ,(smf-color 73))))
+ `(font-lock-comment-face       ((t :foreground ,(smf-color 102) :italic t)))
+ `(font-lock-constant-face      ((t :foreground ,(smf-color 151))))
+ `(font-lock-doc-face           ((t :foreground ,(smf-color 151))))
+ `(font-lock-function-name-face ((t :foreground ,(smf-color 116) :underline t)))
+ `(font-lock-keyword-face       ((t :foreground ,(smf-color 223))))
+ `(font-lock-preprocessor-face  ((t :foreground ,(smf-color 111))))
+ `(font-lock-string-face        ((t :foreground ,(smf-color 174))))
+ `(font-lock-type-face          ((t :foreground ,(smf-color 109))))
+ `(font-lock-variable-name-face ((t :foreground ,(smf-color 181))))
+
+ )
+
+(provide-theme 'smf-zenburn)
