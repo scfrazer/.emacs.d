d913fe8a144dafa639ce66dab5b00793d8a32b54
Scott Frazer <frazer.scott@gmail.com>
2016-11-16 05:49:47 -0800

    Theme tweak

--- themes/smf-base-dark-theme.el
+++ themes/smf-base-dark-theme.el
@@ -10,7 +10,7 @@
    `(default ((((type graphic) (background dark)) (:background "#1d2937" :foreground "#f2f2f2"))))
    `(cursor  ((((type graphic) (background dark)) (:background "#00cd00" :foreground "black"))))
 
-   `(bm-face                        ((t :background ,(smf-color 238))))
+   `(bm-face                        ((t :foreground ,(smf-color 231) :background ,(smf-color 95))))
    `(diff-added                     ((t :foreground "green" :background ,(smf-color 237))))
    `(diff-removed                   ((t :foreground "red" :background ,(smf-color 237))))
    `(error                          ((t :foreground ,(smf-color 210) :background ,(smf-color 52))))
