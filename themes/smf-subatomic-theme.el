1ca3180660245c9225c07015b090dfe10bf0a461
Scott Frazer <frazer.scott@gmail.com>
2016-12-14 07:48:45 -0800

    Add subatomic theme

---
+++ themes/smf-subatomic-theme.el
@@ -0,0 +1,22 @@
+(deftheme smf-subatomic
+  "Subatomic colors.")
+
+(load-theme 'smf-base-dark t)
+
+(custom-theme-set-faces
+ 'smf-subatomic
+
+ `(font-lock-builtin-face       ((t :foreground ,(smf-color 214))))
+ `(font-lock-comment-face       ((t :foreground ,(smf-color 60) :italic t)))
+ `(font-lock-constant-face      ((t :foreground ,(smf-color 15))))
+ `(font-lock-doc-face           ((t :foreground ,(smf-color 133) :italic t)))
+ `(font-lock-function-name-face ((t :foreground ,(smf-color 109) :underline t)))
+ `(font-lock-keyword-face       ((t :foreground ,(smf-color 214))))
+ `(font-lock-preprocessor-face  ((t :foreground ,(smf-color 181))))
+ `(font-lock-string-face        ((t :foreground ,(smf-color 149))))
+ `(font-lock-type-face          ((t :foreground ,(smf-color 110))))
+ `(font-lock-variable-name-face ((t :foreground ,(smf-color 109))))
+
+ )
+
+(provide-theme 'smf-subatomic)
