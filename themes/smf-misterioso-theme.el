651730caea9b08d23526e45124b6994e93976952
Scott Frazer <frazer.scott@gmail.com>
2017-04-07 09:13:45 -0400

    Tweak a theme

--- themes/smf-misterioso-theme.el
+++ themes/smf-misterioso-theme.el
@@ -8,10 +8,10 @@
 
  `(default                      ((t :foreground ,(smf-color 253))))
  `(font-lock-builtin-face       ((t :foreground ,(smf-color 44))))
- `(font-lock-comment-face       ((t :foreground ,(smf-color 108) :italic t)))
+ `(font-lock-comment-face       ((t :foreground ,(smf-color 109) :italic t)))
  `(font-lock-constant-face      ((t :foreground ,(smf-color 37))))
  `(font-lock-doc-face           ((t :foreground ,(smf-color 223) :italic t)))
- `(font-lock-function-name-face ((t :foreground ,(smf-color 50) :underline t)))
+ `(font-lock-function-name-face ((t :foreground ,(smf-color 115) :underline t)))
  `(font-lock-keyword-face       ((t :foreground ,(smf-color 214))))
  `(font-lock-preprocessor-face  ((t :foreground ,(smf-color 220))))
  `(font-lock-string-face        ((t :foreground ,(smf-color 166))))
