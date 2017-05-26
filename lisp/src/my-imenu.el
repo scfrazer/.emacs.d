d517b2d013487c72d21c63c5625d3b01b7c5bc1a
Scott Frazer <frazer.scott@gmail.com>
2017-05-26 12:59:06 -0700

    Pulse imenu tag after jump

--- lisp/src/my-imenu.el
+++ lisp/src/my-imenu.el
@@ -10,6 +10,23 @@
 
 ;; ido + imenu
 
+(defface my-ido-menu-highlight-tag-face
+  '((t (:foreground "white" :background "cadetblue4")))
+  "Font Lock mode face used to highlight tags."
+  :group 'faces)
+
+(defun my-ido-menu-highlight (beg end)
+  "Highlight a region temporarily."
+  (let ((ov (make-overlay beg end))
+        (idx 0))
+    (while (< idx 3)
+      (overlay-put ov 'face 'my-ido-menu-highlight-tag-face)
+      (sit-for 0.3)
+      (overlay-put ov 'face nil)
+      (sit-for 0.2)
+      (setq idx (1+ idx)))
+    (delete-overlay ov)))
+
 (defvar my-ido-imenu-prefix-symbols nil
   "*Prefix symbols with their menu drill-downs.")
 (make-local-variable 'my-ido-imenu-prefix-symbols)
@@ -20,7 +37,8 @@
       (cond ((and (listp symbol) (imenu--subalist-p symbol))
              (setq result (my-ido-imenu-add-symbols (if my-ido-imenu-prefix-symbols
                                                         (concat prefix (car symbol) " > ")
-                                                      "") symbol result)))
+                                                      "")
+                                                    symbol result)))
             ((listp symbol)
              (push (cons (concat (when my-ido-imenu-prefix-symbols prefix) (car symbol)) (cdr symbol)) result))
             ((stringp symbol)
@@ -47,12 +65,23 @@
             (throw 'done t)
           (when (cdr items)
             (setq items (nconc (cdr items) (list (car items))))))))
-    (let ((pos (cdr (assoc (ido-completing-read "Goto symbol: " (mapcar 'car items) nil t) items))))
+    (let* ((item (assoc (ido-completing-read "Goto symbol: " (mapcar 'car items) nil t) items))
+           (name (car item))
+           (len (length name))
+           (pos (cdr item)))
       (when pos
         (let ((recenter (not (pos-visible-in-window-p pos))))
           (goto-char pos)
           (when recenter
-            (recenter)))))))
+            (recenter))
+          (if (looking-at (regexp-quote name))
+              (my-ido-menu-highlight pos (+ pos len))
+            (if (search-forward name (point-at-eol) t)
+                (progn
+                  (goto-char (- (point) len))
+                  (my-ido-menu-highlight (point) (+ (point) len)))
+              (goto-char (point-at-bol))
+              (my-ido-menu-highlight (point) (point-at-eol)))))))))
 
 ;; Better imenu entry point
 
