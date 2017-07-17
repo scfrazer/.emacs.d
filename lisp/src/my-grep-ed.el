55234cb9e7ee925c79057d520a474a4998673cc5
Scott Frazer <frazer.scott@gmail.com>
2017-07-17 10:06:30 -0400

    my-grep-ed: Works with P4

--- lisp/src/my-grep-ed.el
+++ lisp/src/my-grep-ed.el
@@ -3,12 +3,15 @@
 (require 'grep-ed)
 
 (defun my-grep-ed-vc-checkout-function (filename)
-  "Checkout from ClearCase"
-  (when (and (string-match "^/vob/" filename)
-             (not (file-writable-p filename)))
-    (let ((clearcase-checkout-arguments (list "-unreserved" "-nmaster"))
-          (clearcase-suppress-checkout-comments t))
-      (clearcase-commented-checkout filename))))
+  "Checkout from version control"
+  (cond ((and (string-match "^/vob/" filename)
+              (not (file-writable-p filename)))
+         (let ((clearcase-checkout-arguments (list "-unreserved" "-nmaster"))
+               (clearcase-suppress-checkout-comments t))
+           (clearcase-commented-checkout filename)))
+        ((and (p4-with-temp-buffer (list "-s" "files" filename) (looking-at "info:"))
+              (not (file-writable-p filename)))
+         (p4-with-temp-buffer (list "-s" "edit" filename)))))
 
 (setq grep-ed-vc-checkout-function 'my-grep-ed-vc-checkout-function)
 
