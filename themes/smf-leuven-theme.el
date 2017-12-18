56b861fc1b8c434897a18a663422994b9e9d10a8
Scott Frazer <frazer.scott@gmail.com>
2017-12-18 05:55:40 -0800

    Leuven theme changes

--- themes/smf-leuven-theme.el
+++ themes/smf-leuven-theme.el
@@ -8,10 +8,10 @@
 
  `(font-lock-builtin-face       ((t :foreground ,(smf-color 26))))
  `(font-lock-comment-face       ((t :foreground ,(smf-color 245) :italic t)))
- `(font-lock-constant-face      ((t :foreground ,(smf-color 166))))
+ `(font-lock-constant-face      ((t :foreground ,(smf-color 130))))
  `(font-lock-doc-face           ((t :foreground ,(smf-color 22) :italic t)))
  `(font-lock-function-name-face ((t :foreground ,(smf-color 24) :underline t)))
- `(font-lock-keyword-face       ((t :foreground ,(smf-color 21))))
+ `(font-lock-keyword-face       ((t :foreground ,(smf-color 21) :bold t)))
  `(font-lock-preprocessor-face  ((t :foreground ,(smf-color 244))))
  `(font-lock-string-face        ((t :foreground ,(smf-color 28))))
  `(font-lock-type-face          ((t :foreground ,(smf-color 61))))
