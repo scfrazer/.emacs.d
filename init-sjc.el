1e5858f962b36266f191db79e4251cec4e314c2b
Scott Frazer <frazer.scott@gmail.com>
2018-06-28 08:52:56 -0400

    Fix SJC init

--- init-sjc.el
+++ init-sjc.el
@@ -1,16 +1,3 @@
 ;;; init-sjc.el
 
-(setq user-emacs-directory "/auto/asic-home-sjc/scfrazer/.emacs.d/")
-(setq recentf-save-file nil)
-
 (load (concat user-emacs-directory "init.el"))
-
-(setq recentf-save-file
-      (convert-standard-filename
-       (let ((view (and use-clearcase clearcase-setview-viewtag)))
-         (if view
-             (concat "/auto/asic-home-sjc/scfrazer/.recentf-" view)
-           "/auto/asic-home-sjc/scfrazer/.recentf"))))
-(recentf-mode t)
-
-(setq my-cc-mode-ctags-executable "/auto/asic-home-sjc/scfrazer/local/bin/ctags")
