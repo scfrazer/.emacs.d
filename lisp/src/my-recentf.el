6d379ff08871f34bc6fa9c9c21a34c72e6f1dee1
Scott Frazer <frazer.scott@gmail.com>
2019-06-27 10:40:40 -0400

    Better recentf nameing per workspace

--- lisp/src/my-recentf.el
+++ lisp/src/my-recentf.el
@@ -7,16 +7,11 @@
 (add-hook 'midnight-hook 'recentf-cleanup)
 (remove-hook 'midnight-hook 'clean-buffer-list)
 
-;; (require 'my-clearcase)
-;; (defvar clearcase-setview-viewtag)
-
-(when (and recentf-save-file (executable-find "p4"))
-  (setq recentf-save-file
-        (convert-standard-filename
-         (let ((ws (string-trim-right (shell-command-to-string "p4 -F %clientName% -ztag info"))))
-           (if (not (string= ws "*unknown*"))
-               (concat "~/.recentf-" ws)
-             "~/.recentf")))))
+(when recentf-save-file
+  (if (getenv "WORKSPACE")
+      (let ((ws-name (file-name-nondirectory (getenv "WORKSPACE"))))
+        (setq recentf-save-file (convert-standard-filename (concat "~/.recentf/" ws-name))))
+    (setq recentf-save-file (convert-standard-filename (concat "~/.recentf/recentf")))))
 
 (setq recentf-auto-cleanup "11:59pm"
       recentf-exclude (quote ("TAGS" ".*/info/dir" "\\.~.+~" ".*/[0-9]+$"))
