4142628ef9555b9c99106a43b071e67401baa338
Scott Frazer <frazer.scott@gmail.com>
2019-08-15 12:55:37 -0400

    Break out xref stuff

--- lisp/src/my-init.el
+++ lisp/src/my-init.el
@@ -566,44 +566,10 @@ _p_rev       _u_pper              _=_: upper/lower       _r_esolve
   :bind* (("C-c X" . my-xclip-yank)
           ("C-c x" . my-xclip-copy)))
 
-(use-package xref
+(use-package my-xref
   :bind* (("M-&" . my-xref-pop-marker-stack-kill-buffer)
           ("M-*" . xref-pop-marker-stack)
-          ("M-?" . xref-find-definitions))
-  :config
-  (progn
-    (setq default-tags-table-function 'my-tags-table-function
-          tags-add-tables t
-          tags-revert-without-query t)
-    (defun my-tags-table-function ()
-      "Locate any dominating TAGS file."
-      (let ((tags-dir (locate-dominating-file default-directory "TAGS")))
-        (when tags-dir
-          (concat tags-dir "TAGS"))))
-    (defun my-xref-pop-marker-stack-kill-buffer ()
-      (interactive)
-      (let ((buf (current-buffer)))
-        (xref-pop-marker-stack)
-        (unless (equal (current-buffer) buf)
-          (kill-buffer buf))))
-    (defun my-xref-next-line ()
-      (interactive)
-      (end-of-line)
-      (re-search-forward "^\\s-*[0-9]+:" nil t)
-      (beginning-of-line))
-    (defun my-xref-prev-line ()
-      (interactive)
-      (beginning-of-line)
-      (re-search-backward "^\\s-*[0-9]+:" nil t))
-    (defun my-xref--show-xref-buffer (xrefs alist)
-      (with-current-buffer (get-buffer xref-buffer-name)
-        (fit-window-to-buffer nil (/ (frame-height) 2))
-        (my-xref-next-line)))
-    (advice-add #'xref--show-xref-buffer :after #'my-xref--show-xref-buffer)
-    (define-key xref--button-map (kbd "RET") #'xref-quit-and-goto-xref)
-    (define-key xref--button-map (kbd "TAB")  #'xref-goto-xref)
-    (define-key xref--xref-buffer-mode-map (kbd "n") #'my-xref-next-line)
-    (define-key xref--xref-buffer-mode-map (kbd "p") #'my-xref-prev-line)))
+          ("M-?" . xref-find-definitions)))
 
 (use-package yank-target
   :bind-keymap (("C-c y" . yank-target-map))
---
+++ lisp/src/my-xref.el
@@ -0,0 +1,44 @@
+;;; my-xref.el
+
+(require 'xref)
+
+(setq-default default-tags-table-function 'my-tags-table-function
+              tags-add-tables t
+              tags-revert-without-query t)
+
+(defun my-tags-table-function ()
+  "Locate any dominating TAGS file."
+  (let ((tags-dir (locate-dominating-file default-directory "TAGS")))
+    (when tags-dir
+      (concat tags-dir "TAGS"))))
+
+(defun my-xref-pop-marker-stack-kill-buffer ()
+  (interactive)
+  (let ((buf (current-buffer)))
+    (xref-pop-marker-stack)
+    (unless (equal (current-buffer) buf)
+      (kill-buffer buf))))
+
+(defun my-xref-next-line ()
+  (interactive)
+  (end-of-line)
+  (re-search-forward "^\\s-*[0-9]+:" nil t)
+  (beginning-of-line))
+
+(defun my-xref-prev-line ()
+  (interactive)
+  (beginning-of-line)
+  (re-search-backward "^\\s-*[0-9]+:" nil t))
+
+(defun my-xref--show-xref-buffer (xrefs alist)
+  (with-current-buffer (get-buffer xref-buffer-name)
+    (fit-window-to-buffer nil (/ (frame-height) 2))
+    (my-xref-next-line)))
+(advice-add #'xref--show-xref-buffer :after #'my-xref--show-xref-buffer)
+
+(define-key xref--button-map (kbd "RET") #'xref-quit-and-goto-xref)
+(define-key xref--button-map (kbd "TAB")  #'xref-goto-xref)
+(define-key xref--xref-buffer-mode-map (kbd "n") #'my-xref-next-line)
+(define-key xref--xref-buffer-mode-map (kbd "p") #'my-xref-prev-line)
+
+(provide 'my-xref)
