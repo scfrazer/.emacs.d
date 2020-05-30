347bedb09fd167c30402052503d501354bdddad9
Scott Frazer <frazer.scott@gmail.com>
2020-05-30 09:03:49 -0400

    Let's try this

--- custom.el
+++ custom.el
@@ -11,9 +11,6 @@
  '(ahs-inhibit-face-list nil)
  '(ahs-select-invisible (quote skip))
  '(ahs-suppress-log t)
- '(amx-backend (quote ido))
- '(amx-mode t)
- '(amx-show-key-bindings nil)
  '(clean-buffer-list-kill-buffer-names
    (quote
     ("*Help*" "*Apropos*" "*Man " "*Buffer List*" "*Compile-Log*" "*vc*" "*vc-diff*")))
@@ -29,13 +26,14 @@
  '(icomplete-compute-delay 0)
  '(icomplete-delay-completions-threshold 10000)
  '(icomplete-max-delay-chars 0)
+ '(ido-ubiquitous-mode t)
  '(midnight-mode t nil (midnight))
  '(minibuffer-prompt-properties
    (quote
     (read-only t point-entered minibuffer-avoid-prompt face minibuffer-prompt)))
  '(package-selected-packages
    (quote
-    (amx auto-highlight-symbol avy bm browse-kill-ring crontab-mode dash dash-functional deft dired-subtree expand-region fd-dired fill-function-arguments filladapt flymake-easy flymake-eslint flymake-perlcritic fzf git-timemachine goto-chg highlight-indent-guides htmlize hydra icomplete-vertical ido-completing-read+ ido-vertical-mode iflipb json-mode markdown-mode modus-operandi-theme modus-vivendi-theme multiple-cursors orderless popup relint rg sr-speedbar use-package visual-regexp web-beautify web-mode with-editor yaml-mode)))
+    (auto-highlight-symbol avy bm browse-kill-ring crontab-mode dash dash-functional deft dired-subtree expand-region fd-dired fill-function-arguments filladapt flymake-easy flymake-eslint flymake-perlcritic fzf git-timemachine goto-chg highlight-indent-guides htmlize hydra icomplete-vertical ido-completing-read+ ido-vertical-mode iflipb json-mode markdown-mode modus-operandi-theme modus-vivendi-theme multiple-cursors orderless popup relint rg sr-speedbar use-package visual-regexp web-beautify web-mode with-editor yaml-mode)))
  '(read-buffer-completion-ignore-case t)
  '(read-file-name-completion-ignore-case t)
  '(safe-local-variable-values
