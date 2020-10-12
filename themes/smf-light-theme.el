faa03dde5f4969ddb3d8104baedd8c6ce99d58ac
Scott Frazer <frazer.scott@gmail.com>
2020-10-12 12:47:47 -0400

    Update default light theme

--- themes/smf-light-theme.el
+++ themes/smf-light-theme.el
@@ -6,7 +6,7 @@
 (custom-theme-set-faces
  'smf-light
 
- `(font-lock-builtin-face       ((t :foreground "deepskyblue4")))
+ `(font-lock-builtin-face       ((t :foreground "darkgoldenrod4")))
  `(font-lock-comment-face       ((t :foreground "gray66" :italic t)))
  `(font-lock-constant-face      ((t :foreground "purple4" :italic t)))
  `(font-lock-doc-face           ((t :foreground "slategray4" :italic t)))
@@ -14,8 +14,8 @@
  `(font-lock-keyword-face       ((t :foreground "blue4" :bold t)))
  `(font-lock-preprocessor-face  ((t :foreground "yellow4")))
  `(font-lock-string-face        ((t :foreground "violetred4")))
- `(font-lock-type-face          ((t :foreground "cyan4")))
- `(font-lock-variable-name-face ((t :foreground "olivedrab4")))
+ `(font-lock-type-face          ((t :foreground "deepskyblue4")))
+ `(font-lock-variable-name-face ((t :foreground "darkolivegreen")))
 
  )
 
