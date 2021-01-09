be444375fb7620d38ca91e017e597fc15ecde9cf
Scott Frazer <frazer.scott@gmail.com>
2021-01-08 22:10:37 -0500

    Show completions buffer in selectrum

--- custom.el
+++ custom.el
@@ -51,7 +51,6 @@
  '(package-hidden-regexps '("\\`diffview\\'"))
  '(package-selected-packages
    '(which-key modus-operandi-theme modus-vivendi-theme diffview darktooth-theme python marginalia selectrum selectrum-prescient flymake project realgud darkburn-theme beacon goto-last-change reformatter tango-plus-theme avy bm browse-kill-ring dash dash-functional deft dired-subtree fd-dired fill-function-arguments filladapt git-timemachine highlight-indent-guides hydra iflipb json-mode markdown-mode multiple-cursors orderless popup relint rg sr-speedbar use-package visual-regexp web-beautify web-mode with-editor yaml-mode))
- '(prescient-filter-method '(prefix literal))
  '(read-buffer-completion-ignore-case t)
  '(read-file-name-completion-ignore-case t)
  '(safe-local-variable-values
--- lisp/src/my-complete.el
+++ lisp/src/my-complete.el
@@ -12,14 +12,15 @@
 (marginalia-mode)
 
 (setq completion-styles '(orderless)
-      orderless-matching-styles '(orderless-prefixes orderless-regexp)
+      marginalia-annotators '(marginalia-annotators-heavy marginalia-annotators-light)
+      orderless-matching-styles '(orderless-prefixes orderless-literal)
+      prescient-filter-method '(prefix literal)
+      prescient-sort-length-enable nil
       read-buffer-completion-ignore-case t
       read-file-name-completion-ignore-case t
-      marginalia-annotators '(marginalia-annotators-heavy marginalia-annotators-light)
-      prescient-sort-length-enable nil
       selectrum-count-style 'current/matches
-      selectrum-refine-candidates-function #'orderless-filter
-      selectrum-highlight-candidates-function #'orderless-highlight-matches)
+      selectrum-highlight-candidates-function #'orderless-highlight-matches
+      selectrum-refine-candidates-function #'orderless-filter)
 
 (defun my-complete-presorted-completion-table (completions)
   "Keep completion table order."
@@ -62,7 +63,6 @@
           (t (completing-read "Multiple matches: " result-list nil t))))
       (call-interactively 'find-file))))
 
-
 (add-hook 'minibuffer-setup-hook 'my-complete-selectrum-setup 100)
 
 (defun my-complete-selectrum-setup ()
@@ -84,47 +84,27 @@
          'selectrum-insert-current-candidate
        'selectrum-select-current-candidate))))
 
+(defvar my-complete-showing-completions nil)
+
+(defun my-complete-completion-all-completions (orig-fun string table pred point &optional metadata)
+  (if (and selectrum-active-p my-complete-showing-completions)
+      (copy-sequence (selectrum-get-current-candidates t))
+    (apply orig-fun (list string table pred point metadata))))
+
+(advice-add 'completion-all-completions :around #'my-complete-completion-all-completions)
+
+(defun my-complete-show-completions ()
+  (interactive)
+  (let ((marginalia-annotators nil)
+        (my-complete-showing-completions t))
+    (minibuffer-completion-help)))
+
+(define-key selectrum-minibuffer-map (kbd "C-o") 'my-complete-show-completions)
+(define-key selectrum-minibuffer-map (kbd "C-x C-n") 'other-window)
+(define-key selectrum-minibuffer-map (kbd "C-x C-p") (lambda () (interactive (other-window -1))))
 (define-key selectrum-minibuffer-map (kbd "M-N") 'selectrum-next-page)
 (define-key selectrum-minibuffer-map (kbd "M-P") 'selectrum-previous-page)
 (define-key selectrum-minibuffer-map (kbd "M-j") 'my-minibuffer-backward-kill)
 (define-key selectrum-minibuffer-map (kbd "M-k") 'my-minibuffer-forward-kill)
-
-;; (setq-default icomplete-compute-delay 0.2
-;;               icomplete-delay-completions-threshold 100
-;;               icomplete-max-delay-chars 2
-;;               icomplete-tidy-shadowed-file-names t)
-;;
-;; (define-key icomplete-minibuffer-map (kbd "<down>") 'icomplete-forward-completions)
-;; (define-key icomplete-minibuffer-map (kbd "<up>")   'icomplete-backward-completions)
-;; (define-key icomplete-minibuffer-map (kbd "C-j")    'exit-minibuffer)
-;; (define-key icomplete-minibuffer-map (kbd "C-n")    'icomplete-forward-completions)
-;; (define-key icomplete-minibuffer-map (kbd "C-p")    'icomplete-backward-completions)
-;;
-;; (defun my-complete--fido-mode-setup ()
-;;   "Customize icomplete fido mode."
-;;   (setq-local completion-styles '(orderless)))
-;;
-;; (advice-add #'icomplete--fido-mode-setup :after #'my-complete--fido-mode-setup)
-;;
-;; (defun my-complete-vertical-format-completions (reformatted)
-;;   "Customize completions format."
-;;   (save-match-data
-;;     (if (string-match "[[]Matched[]]" reformatted)
-;;         (progn
-;;           (add-face-text-property (match-beginning 0) (match-end 0) 'success nil reformatted)
-;;           (setq reformatted (concat " " reformatted)))
-;;       (when (string-match "[(]No matches[)]" reformatted)
-;;         (add-face-text-property (match-beginning 0) (match-end 0) 'error nil reformatted)
-;;         (setq reformatted (concat " " reformatted)))))
-;;   reformatted)
-;;
-;; (advice-add #'icomplete-vertical-format-completions :filter-return #'my-complete-vertical-format-completions)
-;;
-;; (defun my-completing-read (prompt collection &optional predicate require-match initial-input hist def inherit-input-method)
-;;   "Like `completing-read', but don't sort COLLECTION"
-;;   (completing-read prompt (my-complete-presorted-completion-table collection) predicate require-match initial-input hist def inherit-input-method))
-;;
-;; (icomplete-vertical-mode 1)
-;; (fido-mode 1)
 
 (provide 'my-complete)
