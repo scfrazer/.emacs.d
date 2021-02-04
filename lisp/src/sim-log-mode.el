62880e882a8548555c0bd3ac8e8d42cacd761e78
Scott Frazer <frazer.scott@gmail.com>
2021-02-04 09:13:26 -0500

    Fix sim-log uvm msgs

--- lisp/src/sim-log-mode.el
+++ lisp/src/sim-log-mode.el
@@ -94,12 +94,12 @@
      (5 'sim-log-mode-msg-face))
     ("^\\(UVM_\\(ERROR\\|FATAL\\)\\) @ \\([0-9.]+ [fpnum]?s\\)\\( \\(.+:[0-9]+\\)\\)?: \\([^ ]+\\) \\[\\(.+?\\)\\] \\(.*\\)$"
      (1 'error)
-     (4 'sim-log-mode-timestamp-face)
-     (5 'sim-log-mode-filename-face)
-     (6 'sim-log-mode-path-face)
-     (7 'sim-log-mode-msg-id-face)
-     (8 'sim-log-mode-error-msg-face))
-    ("^\\(UVM_WARNING\\) @ \\([0-9.]+ [fpnum]?s\\)\\( \\[\\(.+:[0-9]+\\)\\]\\)?: \\([^ ]+\\) \\[\\(.+?\\)\\] \\(.*\\)$"
+     (3 'sim-log-mode-timestamp-face)
+     (4 'sim-log-mode-filename-face)
+     (5 'sim-log-mode-path-face)
+     (6 'sim-log-mode-msg-id-face)
+     (7 'sim-log-mode-error-msg-face))
+    ("^\\(UVM_WARNING\\) @ \\([0-9.]+ [fpnum]?s\\)\\( \\(.+:[0-9]+\\)\\)?: \\([^ ]+\\) \\[\\(.+?\\)\\] \\(.*\\)$"
      (1 'warning)
      (2 'sim-log-mode-timestamp-face)
      (3 'sim-log-mode-filename-face)
@@ -107,21 +107,21 @@
      (6 'sim-log-mode-msg-id-face)
      (7 'sim-log-mode-debug-msg-face))
     ;; Standard log
-    ("^\\(UVM_INFO\\)\\( [a-zA-Z0-9_./]+([0-9]+)\\)? @ \\([0-9]+\\): \\([^ ]+\\) \\[\\(.+?\\)\\] \\(.*\\)$"
+    ("^\\(UVM_INFO\\)\\( /[a-zA-Z0-9_./]+([0-9]+)\\)? @ \\([0-9]+\\): \\([^ ]+\\) \\[\\(.+?\\)\\] \\(.*\\)$"
      (1 'sim-log-mode-info-face)
      (2 'sim-log-mode-filename-face)
      (3 'sim-log-mode-timestamp-face)
      (4 'sim-log-mode-path-face)
      (5 'sim-log-mode-msg-id-face)
      (6 'sim-log-mode-msg-face))
-    ("^\\(UVM_\\(ERROR\\|FATAL\\)\\)\\( [a-zA-Z0-9_./]+([0-9]+)\\)? @ \\([0-9]+\\): \\([^ ]+\\) \\(.+?\\) \\(.*\\)$"
+    ("^\\(UVM_\\(ERROR\\|FATAL\\)\\)\\( /[a-zA-Z0-9_./]+([0-9]+)\\)? @ \\([0-9]+\\): \\([^ ]+\\) \\(.+?\\) \\(.*\\)$"
      (1 'error)
      (3 'sim-log-mode-filename-face)
      (4 'sim-log-mode-timestamp-face)
      (5 'sim-log-mode-path-face)
      (6 'sim-log-mode-msg-id-face)
      (7 'sim-log-mode-msg-face))
-    ("^\\(UVM_WARNING\\)\\( [a-zA-Z0-9_./]+([0-9]+)\\)? @ \\([0-9]+\\): \\([^ ]+\\) \\[\\(.+?\\)\\] \\(.*\\)$"
+    ("^\\(UVM_WARNING\\)\\( /[a-zA-Z0-9_./]+([0-9]+)\\)? @ \\([0-9]+\\): \\([^ ]+\\) \\[\\(.+?\\)\\] \\(.*\\)$"
      (1 'warning)
      (2 'sim-log-mode-filename-face)
      (3 'sim-log-mode-timestamp-face)
