5b8466ab11f50de923a73972fb48fcd8d49d7b7b
Scott Frazer <frazer.scott@gmail.com>
2021-02-17 08:58:12 -0500

    Update pkgs

--- elpa/selectrum-20210216.2234/selectrum-pkg.el
+++ elpa/selectrum-20210216.2234/selectrum-pkg.el
@@ -1,6 +1,6 @@
-(define-package "selectrum" "20210215.1046" "Easily select item from list"
+(define-package "selectrum" "20210216.2234" "Easily select item from list"
   '((emacs "25.1"))
-  :commit "af5d027681d18d10559c2cddefe57c5577bbd611" :authors
+  :commit "ad9d9f02d6ac916cf4caa548c1967457bfc37c02" :authors
   '(("Radon Rosborough" . "radon.neon@gmail.com"))
   :maintainer
   '("Radon Rosborough" . "radon.neon@gmail.com")
