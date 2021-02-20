b9c2b1b05463187e4a61915ccf143cd2b462e1f8
Scott Frazer <frazer.scott@gmail.com>
2021-02-20 07:52:08 -0500

    New experimental light theme

---
+++ themes/smf-light2-theme.el
@@ -0,0 +1,22 @@
+(deftheme smf-light2
+  "Light colors.")
+
+(load-theme 'smf-base-light t)
+
+(custom-theme-set-faces
+ 'smf-light2
+
+ `(font-lock-builtin-face       ((t :foreground "lightsalmon4" :bold t)))
+ `(font-lock-comment-face       ((t :foreground "gray66" :italic t)))
+ `(font-lock-constant-face      ((t :foreground "purple4" :italic t)))
+ `(font-lock-doc-face           ((t :foreground "slategray4" :italic t)))
+ `(font-lock-function-name-face ((t :foreground "red4" :underline t)))
+ `(font-lock-keyword-face       ((t :foreground "#204a87" :bold t)))
+ `(font-lock-preprocessor-face  ((t :foreground "yellow4")))
+ `(font-lock-string-face        ((t :foreground "purple4")))
+ `(font-lock-type-face          ((t :foreground "deepskyblue4")))
+ `(font-lock-variable-name-face ((t :foreground "darkgreen")))
+
+ )
+
+(provide-theme 'smf-light2)
