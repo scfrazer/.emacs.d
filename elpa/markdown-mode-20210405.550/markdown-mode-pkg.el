770069202a4ce2d1ff6d4466dc6434df7f3c7f8c
Scott Frazer <frazer.scott@gmail.com>
2021-04-05 09:26:52 -0400

    Update pkgs

---
+++ elpa/markdown-mode-20210405.550/markdown-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from markdown-mode.el  -*- no-byte-compile: t -*-
+(define-package "markdown-mode" "20210405.550" "Major mode for Markdown-formatted text" '((emacs "25.1")) :commit "f18d26d2a7de425aaa142f7fa8338391f2df0f73" :authors '(("Jason R. Blevins" . "jblevins@xbeta.org")) :maintainer '("Jason R. Blevins" . "jblevins@xbeta.org") :keywords '("markdown" "github flavored markdown" "itex") :url "https://jblevins.org/projects/markdown-mode/")
