db250d1bbb4258923561770d552ad8e7b76765aa
Scott Frazer <frazer.scott@gmail.com>
2021-04-18 20:08:44 -0400

    Remove unused packages

--- elpa/eglot-20210413.21/eglot-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from eglot.el  -*- no-byte-compile: t -*-
-(define-package "eglot" "20210413.21" "Client for Language Server Protocol (LSP) servers" '((emacs "26.1") (jsonrpc "1.0.14") (flymake "1.0.9") (project "0.3.0") (xref "1.0.1") (eldoc "1.11.0")) :commit "05fe6472cb1766e4dafae7562b8793d96d0bb271" :authors '(("João Távora" . "joaotavora@gmail.com")) :maintainer '("João Távora" . "joaotavora@gmail.com") :keywords '("convenience" "languages") :url "https://github.com/joaotavora/eglot")
--- package-quickstart.el
+++ package-quickstart.el
@@ -933,40 +933,6 @@ See info node `(transient)Modifying Existing Transients'.
 
 
 )
-(let ((load-file-name "/home/scfrazer/.emacs.d/elpa/test-simple-20200722.1121/test-simple-autoloads.el"))
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory "/home/scfrazer/.emacs.d/elpa/test-simple-20200722.1121/test-simple-autoloads.el") (car load-path))))
-
-
-
-(autoload 'test-simple-start "test-simple" "\
-
-
-\(fn &optional TEST-START-MSG)" nil t)
-
-(autoload 'test-simple-clear "test-simple" "\
-Initialize and reset everything to run tests.
-You should run this before running any assertions.  Running more than once
-clears out information from the previous run.
-
-\(fn &optional TEST-INFO TEST-START-MSG)" t nil)
-
-(autoload 'test-simple-run "test-simple" "\
-Register command line to run tests non-interactively and bind key to run test.
-After calling this function, you can run test by key specified by `test-simple-runner-key'.
-
-It is preferable to write at the first line of test files as a comment, e.g,
-;;;; (test-simple-run \"emacs -batch -L %s -l %s\" (file-name-directory (locate-library \"test-simple.elc\")) buffer-file-name)
-
-Calling this function interactively, COMMAND-LINE-FORMATS is set above.
-
-\(fn &rest COMMAND-LINE-FORMATS)" t nil)
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "test-simple" '("assert-" "end-tests" "note" "test-simple-")))
-
-
-)
 (let ((load-file-name "/home/scfrazer/.emacs.d/elpa/tango-plus-theme-20201103.903/tango-plus-theme-autoloads.el"))
 
 (add-to-list 'load-path (directory-file-name
@@ -1486,136 +1452,6 @@ EXIT-CODE-SUCCESS-P
 (function-put 'reformatter-define 'lisp-indent-function 'defun)
 
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "reformatter" '("reformatter-")))
-
-
-)
-(let ((load-file-name "/home/scfrazer/.emacs.d/elpa/load-relative-20201130.2202/load-relative-autoloads.el"))
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory "/home/scfrazer/.emacs.d/elpa/load-relative-20201130.2202/load-relative-autoloads.el") (car load-path))))
-
-
-
-(autoload '__FILE__ "load-relative" "\
-Return the string name of file/buffer that is currently begin executed.
-
-The first approach for getting this information is perhaps the
-most pervasive and reliable.  But it the most low-level and not
-part of a public API, so it might change in future
-implementations.  This method uses the name that is recorded by
-readevalloop of `lread.c' as the car of variable
-`current-load-list'.
-
-Failing that, we use `load-file-name' which should work in some
-subset of the same places that the first method works.  However
-`load-file-name' will be nil for code that is eval'd.  To cover
-those cases, we try function `buffer-file-name' which is initially
-correct, for eval'd code, but will change and may be wrong if the
-code sets or switches buffers after the initial execution.
-
-As a last resort, you can pass in SYMBOL which should be some
-symbol that has been previously defined if none of the above
-methods work we will use the file-name value find via
-`symbol-file'.
-
-\(fn &optional SYMBOL)" nil nil)
-
-(autoload 'find-file-noselect-relative "load-relative" "\
-Read relative FILENAME into a buffer and return the buffer.
-If a buffer exists visiting FILENAME, return that one, but
-verify that the file has not changed since visited or saved.
-The buffer is not selected, just returned to the caller.
-Optional second arg NOWARN non-nil means suppress any warning messages.
-Optional third arg RAWFILE non-nil means the file is read literally.
-Optional fourth arg WILDCARDS non-nil means do wildcard processing
-and visit all the matching files.  When wildcards are actually
-used and expanded, return a list of buffers that are visiting
-the various files.
-
-\(fn FILENAME &optional NOWARN RAWFILE WILDCARDS)" nil nil)
-
-(autoload 'with-relative-file "load-relative" "\
-Read the relative FILE into a temporary buffer and evaluate BODY
-in this buffer.
-
-\(fn FILE &rest BODY)" nil t)
-
-(function-put 'with-relative-file 'lisp-indent-function '1)
-
-(autoload 'load-relative "load-relative" "\
-Load an Emacs Lisp file relative to Emacs Lisp code that is in
-the process of being loaded or eval'd.
-
-FILE-OR-LIST is either a string or a list of strings containing
-files that you want to loaded.  If SYMBOL is given, the location of
-of the file of where that was defined (as given by `symbol-file' is used
-if other methods of finding __FILE__ don't work.
-
-\(fn FILE-OR-LIST &optional SYMBOL)" nil nil)
-
-(autoload 'require-relative "load-relative" "\
-Run `require' on an Emacs Lisp file relative to the Emacs Lisp code
-that is in the process of being loaded or eval'd.  The symbol used in require
-is the base file name (without directory or file extension) treated as a
-symbol.
-
-WARNING: it is best to to run this function before any
-buffer-setting or buffer changing operations.
-
-\(fn RELATIVE-FILE &optional OPT-FILE OPT-PREFIX)" nil nil)
-
-(autoload 'require-relative-list "load-relative" "\
-Run `require-relative' on each name in LIST which should be a list of
-strings, each string being the relative name of file you want to run.
-
-\(fn LIST &optional OPT-PREFIX)" nil t)
-
-(autoload 'provide-me "load-relative" "\
-Call `provide' with the feature's symbol name made from
-source-code's file basename sans extension.  For example if you
-write (provide-me) inside file ~/lisp/foo.el, this is the same as
-writing: (provide \\='foo).
-
-With a prefix, that prefix is prepended to the `provide' So in
-the previous example, if you write (provide-me \"bar-\") this is the
-same as writing (provide \\='bar-foo).
-
-\(fn &optional PREFIX)" nil t)
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "load-relative" '("autoload-relative")))
-
-
-
-
-)
-(let ((load-file-name "/home/scfrazer/.emacs.d/elpa/loc-changes-20200722.1111/loc-changes-autoloads.el"))
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory "/home/scfrazer/.emacs.d/elpa/loc-changes-20200722.1111/loc-changes-autoloads.el") (car load-path))))
-
-
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "loc-changes" '("loc-changes")))
-
-
-)
-(let ((load-file-name "/home/scfrazer/.emacs.d/elpa/realgud-20210417.1421/realgud-autoloads.el"))
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory "/home/scfrazer/.emacs.d/elpa/realgud-20210417.1421/realgud-autoloads.el") (car load-path))))
-
-
-
-(defconst realgud--recursive-autoloads-file-name "realgud-recursive-autoloads.el" "\
-Where to store autoloads for subdirectory contents.")
-
-(defconst realgud--recursive-autoloads-base-directory (file-name-directory (if load-in-progress load-file-name buffer-file-name)))
-
-(with-demoted-errors "Error in RealGUD's autoloads: %s" (load (expand-file-name realgud--recursive-autoloads-file-name realgud--recursive-autoloads-base-directory) t t))
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "realgud" '("realgud--rebuild-recursive-autoloads")))
-
-
 
 
 )
@@ -2355,17 +2191,6 @@ Cycle between annotators in `marginalia-annotators'." t nil)
 
 
 )
-(let ((load-file-name "/home/scfrazer/.emacs.d/elpa/jsonrpc-1.0.14/jsonrpc-autoloads.el"))
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory "/home/scfrazer/.emacs.d/elpa/jsonrpc-1.0.14/jsonrpc-autoloads.el") (car load-path))))
-
-
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "jsonrpc" '("jsonrpc-")))
-
-
-)
 (let ((load-file-name "/home/scfrazer/.emacs.d/elpa/json-snatcher-20200916.1717/json-snatcher-autoloads.el"))
 
 (add-to-list 'load-path (directory-file-name
@@ -2881,53 +2706,6 @@ The command run (after changing into DIR) is
 \(fn DIR REGEXP)" t nil)
 
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "fd-dired" '("fd-")))
-
-
-)
-(let ((load-file-name "/home/scfrazer/.emacs.d/elpa/eglot-20210413.21/eglot-autoloads.el"))
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory "/home/scfrazer/.emacs.d/elpa/eglot-20210413.21/eglot-autoloads.el") (car load-path))))
-
-
-
-(autoload 'eglot "eglot" "\
-Manage a project with a Language Server Protocol (LSP) server.
-
-The LSP server of CLASS is started (or contacted) via CONTACT.
-If this operation is successful, current *and future* file
-buffers of MANAGED-MAJOR-MODE inside PROJECT become \"managed\"
-by the LSP server, meaning information about their contents is
-exchanged periodically to provide enhanced code-analysis via
-`xref-find-definitions', `flymake-mode', `eldoc-mode',
-`completion-at-point', among others.
-
-Interactively, the command attempts to guess MANAGED-MAJOR-MODE
-from current buffer, CLASS and CONTACT from
-`eglot-server-programs' and PROJECT from `project-current'.  If
-it can't guess, the user is prompted.  With a single
-\\[universal-argument] prefix arg, it always prompt for COMMAND.
-With two \\[universal-argument] prefix args, also prompts for
-MANAGED-MAJOR-MODE.
-
-PROJECT is a project instance as returned by `project-current'.
-
-CLASS is a subclass of `eglot-lsp-server'.
-
-CONTACT specifies how to contact the server.  It is a
-keyword-value plist used to initialize CLASS or a plain list as
-described in `eglot-server-programs', which see.
-
-INTERACTIVE is t if called interactively.
-
-\(fn MANAGED-MAJOR-MODE PROJECT CLASS CONTACT &optional INTERACTIVE)" t nil)
-
-(autoload 'eglot-ensure "eglot" "\
-Start Eglot session for current buffer if there isn't one." nil nil)
-
-(put 'eglot-workspace-configuration 'safe-local-variable 'listp)
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "eglot" '("eglot-")))
 
 
 )
@@ -3549,7 +3327,7 @@ Transpose lines in the active region." t nil)
 )
 (setq package-activated-list
       (append
-       '(yaml-mode xref xr with-editor which-key wgrep web-mode web-beautify visual-regexp bind-key use-package tron-legacy-theme transient test-simple tango-plus-theme sr-speedbar prescient selectrum selectrum-prescient rg relint reformatter load-relative loc-changes realgud python project popup orderless multiple-cursors monokai-pro-theme markdown-mode marginalia lv jsonrpc json-snatcher json-reformat json-mode iflipb hydra highlight-indent-guides goto-last-change git-timemachine eldoc flymake filladapt fill-function-arguments fd-dired eglot dash dired-hacks-utils dired-subtree diffview deft dash-functional browse-kill-ring bm beacon avy)
+       '(yaml-mode xref xr with-editor which-key wgrep web-mode web-beautify visual-regexp bind-key use-package tron-legacy-theme transient tango-plus-theme sr-speedbar prescient selectrum selectrum-prescient rg relint reformatter python project popup orderless multiple-cursors monokai-pro-theme markdown-mode marginalia lv json-snatcher json-reformat json-mode iflipb hydra highlight-indent-guides goto-last-change git-timemachine eldoc flymake filladapt fill-function-arguments fd-dired dash dired-hacks-utils dired-subtree diffview deft dash-functional browse-kill-ring bm beacon avy)
        package-activated-list))
 (progn
   (require 'info)
