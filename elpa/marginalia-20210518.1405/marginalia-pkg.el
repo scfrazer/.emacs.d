343c14b96f7550d03172d58e87c9549f197bf793
Scott Frazer <frazer.scott@gmail.com>
2021-05-18 12:54:00 -0400

    Update pkgs

--- elpa/marginalia-20210518.1405/marginalia-pkg.el
+++ elpa/marginalia-20210518.1405/marginalia-pkg.el
@@ -1,6 +1,6 @@
-(define-package "marginalia" "20210506.1500" "Enrich existing commands with completion annotations"
+(define-package "marginalia" "20210518.1405" "Enrich existing commands with completion annotations"
   '((emacs "26.1"))
-  :commit "445d2832a2f06484ad28d9b55676c52d63cd0a46" :authors
+  :commit "624028c69b55deb3387452b9eeabe9cb963bd2a4" :authors
   '(("Omar Antolín Camarena <omar@matem.unam.mx>, Daniel Mendler" . "mail@daniel-mendler.de"))
   :maintainer
   '("Omar Antolín Camarena <omar@matem.unam.mx>, Daniel Mendler" . "mail@daniel-mendler.de")
--- elpa/orderless-20210518.1413/orderless-pkg.el
+++ elpa/orderless-20210518.1413/orderless-pkg.el
@@ -1,6 +1,6 @@
-(define-package "orderless" "20210517.1413" "Completion style for matching regexps in any order"
+(define-package "orderless" "20210518.1413" "Completion style for matching regexps in any order"
   '((emacs "24.4"))
-  :commit "94e8940ac1ad5ec230347c36a1bbdca42de973cf" :authors
+  :commit "9637d7fd59f76a5b6d37470b1543ab827a0f9b8d" :authors
   '(("Omar Antolín Camarena" . "omar@matem.unam.mx"))
   :maintainer
   '("Omar Antolín Camarena" . "omar@matem.unam.mx")
