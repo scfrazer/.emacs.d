17561b3d4b68c7d9680856fbe62fbe9a64766252
Scott Frazer <frazer.scott@gmail.com>
2021-06-23 06:58:01 -0400

    Update pkgs

--- elpa/which-key-20210622.334/which-key-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from which-key.el  -*- no-byte-compile: t -*-
-(define-package "which-key" "20210622.334" "Display available keybindings in popup" '((emacs "24.4")) :commit "28f386cc4af8c0fe21269bb587a5bb229ba3834e" :authors '(("Justin Burkett" . "justin@burkett.cc")) :maintainer '("Justin Burkett" . "justin@burkett.cc") :url "https://github.com/justbur/emacs-which-key")
