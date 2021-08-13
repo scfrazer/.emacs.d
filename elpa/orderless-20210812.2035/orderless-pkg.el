df3192767bac7fc14a0c394a4c87aa4e5477345c
Scott Frazer <frazer.scott@gmail.com>
2021-08-13 08:59:46 -0400

    Pkgs

--- elpa/orderless-20210812.2035/orderless-pkg.el
+++ elpa/orderless-20210812.2035/orderless-pkg.el
@@ -1,6 +1,6 @@
-(define-package "orderless" "20210811.1403" "Completion style for matching regexps in any order"
+(define-package "orderless" "20210812.2035" "Completion style for matching regexps in any order"
   '((emacs "26.1"))
-  :commit "665ade998466c674a566daaa095510e39fbe738e" :authors
+  :commit "1a7011ac9c476dbb083c5ead88462a5f520ef8aa" :authors
   '(("Omar Antolín Camarena" . "omar@matem.unam.mx"))
   :maintainer
   '("Omar Antolín Camarena" . "omar@matem.unam.mx")
