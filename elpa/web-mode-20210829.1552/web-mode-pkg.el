610e9ea0b6df1132995800dc31fa7e992a1c182c
Scott Frazer <frazer.scott@gmail.com>
2021-09-03 09:15:59 -0400

    Pkgs

--- elpa/web-mode-20210829.1552/web-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from web-mode.el  -*- no-byte-compile: t -*-
-(define-package "web-mode" "20210829.1552" "major mode for editing web templates" '((emacs "23.1")) :commit "950ac58bd9e7d14a63761c7c8c245d0fc04595c5" :authors '(("François-Xavier Bois <fxbois AT Google Mail Service>")) :maintainer '("François-Xavier Bois") :keywords '("languages") :url "https://web-mode.org")
