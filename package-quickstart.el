b75e49ffc4731efdb1fbc4bf7cc1369fca249c1e
Scott Frazer <frazer.scott@gmail.com>
2023-04-21 08:37:41 -0400

    Pkgs

--- package-quickstart.el
+++ package-quickstart.el
@@ -2204,10 +2204,10 @@ and toggle it if ARG is `toggle'; disable the mode otherwise.
 
 
 )
-(let ((load-file-name "/home/scfrazer/.emacs.d/elpa/marginalia-20230417.1829/marginalia-autoloads.el"))
+(let ((load-file-name "/home/scfrazer/.emacs.d/elpa/marginalia-20230421.841/marginalia-autoloads.el"))
 
 (add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory "/home/scfrazer/.emacs.d/elpa/marginalia-20230417.1829/marginalia-autoloads.el") (car load-path))))
+                         (or (file-name-directory "/home/scfrazer/.emacs.d/elpa/marginalia-20230421.841/marginalia-autoloads.el") (car load-path))))
 
 
 
