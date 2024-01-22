eb98044a0aa9cfd9f539911ba5b8dd2a5c2c6bb2
Scott Frazer <frazer.scott@gmail.com>
2024-01-22 14:16:44 -0500

    Pkgs

--- elpa/transient-20240120.1944/transient-pkg.el
+++ elpa/transient-20240120.1944/transient-pkg.el
@@ -1,8 +1,8 @@
-(define-package "transient" "20240106.1457" "Transient commands"
+(define-package "transient" "20240120.1944" "Transient commands"
   '((emacs "26.1")
     (compat "29.1.4.4")
     (seq "2.24"))
-  :commit "a2fe06ac00d419ddcbf8f9ef59bf1c4d2350ccbe" :authors
+  :commit "f51c144a47a6a865e6453afb82c3c12a31fa4b0b" :authors
   '(("Jonas Bernoulli" . "jonas@bernoul.li"))
   :maintainers
   '(("Jonas Bernoulli" . "jonas@bernoul.li"))
