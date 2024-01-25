570ef081327d76493169b4f64bcbf75c37546c1d
Scott Frazer <frazer.scott@gmail.com>
2024-01-25 09:06:47 -0500

    Pkgs

--- elpa/vertico-20240122.2326/vertico-pkg.el
+++ elpa/vertico-20240122.2326/vertico-pkg.el
@@ -1,7 +1,7 @@
-(define-package "vertico" "20240103.1359" "VERTical Interactive COmpletion"
+(define-package "vertico" "20240122.2326" "VERTical Interactive COmpletion"
   '((emacs "27.1")
     (compat "29.1.4.4"))
-  :commit "357c2503a7787193d50dafd8212f2b1cc165ae16" :authors
+  :commit "cf8b2abf5207696c054c33214c86e3969d415054" :authors
   '(("Daniel Mendler" . "mail@daniel-mendler.de"))
   :maintainers
   '(("Daniel Mendler" . "mail@daniel-mendler.de"))
