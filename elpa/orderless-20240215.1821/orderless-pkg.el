5253debb595d4c6bd2c9e9231084da80b1249252
Scott Frazer <frazer.scott@gmail.com>
2024-02-16 08:18:07 -0500

    Pkgs

--- elpa/orderless-20240215.1821/orderless-pkg.el
+++ elpa/orderless-20240215.1821/orderless-pkg.el
@@ -1,6 +1,6 @@
-(define-package "orderless" "20240215.1806" "Completion style for matching regexps in any order"
+(define-package "orderless" "20240215.1821" "Completion style for matching regexps in any order"
   '((emacs "27.1"))
-  :commit "2c952fd161f95dd2b92aff5ffaeb50ab4e39ed10" :authors
+  :commit "61aed3e622f90ee8ace44a84a05abc1074567b74" :authors
   '(("Omar Antolín Camarena" . "omar@matem.unam.mx"))
   :maintainers
   '(("Omar Antolín Camarena" . "omar@matem.unam.mx"))
--- package-quickstart.el
+++ package-quickstart.el
@@ -1867,10 +1867,10 @@ would otherwise have the same name.
 
 
 )
-(let ((load-file-name "/home/scfrazer/.emacs.d/elpa/orderless-20240215.1806/orderless-autoloads.el"))
+(let ((load-file-name "/home/scfrazer/.emacs.d/elpa/orderless-20240215.1821/orderless-autoloads.el"))
 
 (add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory "/home/scfrazer/.emacs.d/elpa/orderless-20240215.1806/orderless-autoloads.el") (car load-path))))
+                         (or (file-name-directory "/home/scfrazer/.emacs.d/elpa/orderless-20240215.1821/orderless-autoloads.el") (car load-path))))
 
 
 
@@ -3495,7 +3495,7 @@ Transpose lines in the active region." t nil)
   (info-initialize)
   (setq Info-directory-list
         (append
-         '("/home/scfrazer/.emacs.d/elpa/dash-20240103.1301" "/home/scfrazer/.emacs.d/elpa/modus-themes-20240212.806" "/home/scfrazer/.emacs.d/elpa/orderless-20240215.1806" "/home/scfrazer/.emacs.d/elpa/rg-20231202.1023" "/home/scfrazer/.emacs.d/elpa/transient-20240201.1100" "/home/scfrazer/.emacs.d/elpa/use-package-20230426.2324" "/home/scfrazer/.emacs.d/elpa/with-editor-20240101.2226" "/home/scfrazer/.emacs.d/elpa/compat-29.1.4.4")
+         '("/home/scfrazer/.emacs.d/elpa/dash-20240103.1301" "/home/scfrazer/.emacs.d/elpa/modus-themes-20240212.806" "/home/scfrazer/.emacs.d/elpa/orderless-20240215.1821" "/home/scfrazer/.emacs.d/elpa/rg-20231202.1023" "/home/scfrazer/.emacs.d/elpa/transient-20240201.1100" "/home/scfrazer/.emacs.d/elpa/use-package-20230426.2324" "/home/scfrazer/.emacs.d/elpa/with-editor-20240101.2226" "/home/scfrazer/.emacs.d/elpa/compat-29.1.4.4")
          Info-directory-list)))
 
 ;; Local Variables:
