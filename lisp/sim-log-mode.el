e41a4b759dc5ea92182d327ed271b015c7279c63
Scott Frazer <frazer.scott@gmail.com>
2024-04-17 10:47:17 -0400

    sim-log update

--- lisp/sim-log-mode.el
+++ lisp/sim-log-mode.el
@@ -93,7 +93,7 @@
     ;;  (4 'sim-log-mode-msg-id-face)
     ;;  (5 'sim-log-mode-msg-face))
     ;; Standard log
-    ("^\\(UVM_INFO\\)\\( /[-a-zA-Z0-9_./]+([0-9]+)\\)? @ ?\\([0-9.]+\\)\\( [fpnum]?s\\)?: \\([^ ]+?\\) \\[\\(.+?\\)\\]\\(.*\\)$"
+    ("^\\(UVM_INFO\\)\\( /[-a-zA-Z0-9_./]+([0-9]+)\\)? @ ?\\([0-9.]+\\)\\( ?[fpnum]?s\\)?: \\([^ ]+?\\) \\[\\(.+?\\)\\]\\(.*\\)$"
      (1 'sim-log-mode-info-face)
      (2 'sim-log-mode-filename-face t t)
      (3 'sim-log-mode-timestamp-face)
@@ -101,7 +101,7 @@
      (5 'sim-log-mode-path-face)
      (6 'sim-log-mode-msg-id-face)
      (7 'sim-log-mode-msg-face))
-    ("^\\(UVM_\\(ERROR\\|FATAL\\)\\)\\( /[-a-zA-Z0-9_./]+([0-9]+)\\) @ ?\\([0-9.]+\\)\\( [fpnum]?s\\)?: \\([^ ]+?\\) \\[\\(.+?\\)\\]\\(.*\\)$"
+    ("^\\(UVM_\\(ERROR\\|FATAL\\)\\)\\( /[-a-zA-Z0-9_./]+([0-9]+)\\) @ ?\\([0-9.]+\\)\\( ?[fpnum]?s\\)?: \\([^ ]+?\\) \\[\\(.+?\\)\\]\\(.*\\)$"
      (1 'error)
      (3 'sim-log-mode-filename-face)
      (4 'sim-log-mode-timestamp-face)
@@ -109,7 +109,7 @@
      (6 'sim-log-mode-path-face)
      (7 'sim-log-mode-msg-id-face)
      (8 'sim-log-mode-error-msg-face))
-    ("^\\(UVM_WARNING\\)\\( /[-a-zA-Z0-9_./]+([0-9]+)\\)? @ ?\\([0-9.]+\\)\\( [fpnum]?s\\)?: \\([^ ]+?\\) \\[\\(.+?\\)\\]\\(.*\\)$"
+    ("^\\(UVM_WARNING\\)\\( /[-a-zA-Z0-9_./]+([0-9]+)\\)? @ ?\\([0-9.]+\\)\\( ?[fpnum]?s\\)?: \\([^ ]+?\\) \\[\\(.+?\\)\\]\\(.*\\)$"
      (1 'warning)
      (2 'sim-log-mode-filename-face t t)
      (3 'sim-log-mode-timestamp-face)
