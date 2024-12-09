adf7a542f7e8f2d323c683d09820d5ba1d7d503d
Scott Frazer <frazer.scott@gmail.com>
2024-12-09 16:51:41 -0500

    Pkgs

---
+++ elpa/modus-themes-20241120.542/modus-themes-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "modus-themes" "20241120.542"
+  "Elegant, highly legible and customizable themes."
+  '((emacs "28.1"))
+  :url "https://github.com/protesilaos/modus-themes"
+  :commit "df1798234edd56678da975d0d65b64bdebafc314"
+  :revdesc "df1798234edd"
+  :keywords '("faces" "theme" "accessibility")
+  :authors '(("Protesilaos Stavrou" . "info@protesilaos.com"))
+  :maintainers '(("Protesilaos Stavrou" . "info@protesilaos.com")))
