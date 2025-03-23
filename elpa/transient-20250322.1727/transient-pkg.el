982066c4ecc844e250dfc7c0219170b022f3401e
Scott Frazer <frazer.scott@gmail.com>
2025-03-23 15:49:13 -0400

    Pkgs

--- elpa/transient-20250322.1727/transient-pkg.el
+++ elpa/transient-20250322.1727/transient-pkg.el
@@ -1,12 +1,12 @@
 ;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "transient" "20250320.1937"
+(define-package "transient" "20250322.1727"
   "Transient commands."
   '((emacs  "26.1")
     (compat "30.0.0.0")
     (seq    "2.24"))
   :url "https://github.com/magit/transient"
-  :commit "55050b60da8b1c5efedf45c885e5e8daf26e2499"
-  :revdesc "55050b60da8b"
+  :commit "14b39ff70609a9f04915c09c7aa13b24a05da848"
+  :revdesc "14b39ff70609"
   :keywords '("extensions")
   :authors '(("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev"))
   :maintainers '(("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev")))
